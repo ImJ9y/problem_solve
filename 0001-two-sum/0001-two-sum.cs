788c8d8d97cfb02eecbce26b30d3ff9d76a06965
ImJ9y <149026501+ImJ9y@users.noreply.github.com>
2023-11-26 19:15:52 -0800

    Time: 199 ms (7.43%), Space: 43.7 MB (98.95%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.Length; i++){
+            for(int j = 1; j < nums.Length; j++){
+                if(i==j){
+                    continue;
+                }
+                if(nums[i] + nums[j] == target){
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new ArgumentException("No solution found");
+    }
+}
