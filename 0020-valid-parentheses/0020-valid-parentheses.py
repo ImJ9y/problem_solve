a59b733474c0e6c23bbe131cbbf20dd07e17f3c0
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-05 22:48:20 -0800

    Time: 17 ms (43.35%), Space: 13.5 MB (33.82%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,4 +1,3 @@
-
 class Solution(object):
     def isValid(self, s):
         """
@@ -8,24 +7,27 @@ class Solution(object):
         checkList = []
         openSymbol = ["(","[","{"]
         closeSymbol = [")","]","}"]
-
-        if len(s)%2 != 0:
+        
+        if len(s) % 2 != 0:
             return False
-        else:
-            for symbol in s:
-                if symbol in openSymbol:
+        
+        for symbol in s:
+            if symbol in openSymbol:
+                checkList.append(symbol)
+            else:
+                if len(checkList) == 0:
+                    return False
+                elif checkList[-1] == "{" and symbol == "}":
+                    checkList.pop()
+                elif checkList[-1] == "[" and symbol == "]":
+                    checkList.pop()
+                elif checkList[-1] == "(" and symbol == ")":
+                    checkList.pop()
+                elif symbol in closeSymbol:
                     checkList.append(symbol)
-                else:
-                    if len(checkList) == 0:
-                        checkList.append(symbol)
-                    elif symbol in closeSymbol:
-                        if checkList[-1] == "{" and symbol == "}":
-                            checkList.pop()
-                        elif checkList[-1] == "[" and symbol == "]":
-                            checkList.pop()
-                        elif checkList[-1] == "(" and symbol == ")":
-                            checkList.pop()
-                        else:
-                            checkList.append(symbol)
-
+        
         return not checkList
+            
+                    
+                            
+        
