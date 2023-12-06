2d3399c10e7384c7d1df3e5b7834d6cd7aa696ef
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-05 21:27:13 -0800

    Time: 15 ms (73.83%), Space: 13.8 MB (14.53%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,15 +4,14 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        if not strs: #if strs is empty - return ""
+        if not strs:
             return ""
         
-        checkWord = strs[0]
+        prefix = strs[0]
         
         for word in strs:
-            #if word finds checkWord return 0 if word didn't find the checkWorkd return -1
-            while word.find(checkWord) != 0: 
-                checkWord = checkWord[:-1]
-                if not checkWord: #if we removed all the char return ""
-                    return ""
-        return checkWord
+            while word.find(prefix) != 0:
+                prefix = prefix[:-1]
+                
+        return prefix
+        
