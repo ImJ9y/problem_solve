ac2b48258a4b571e6a754d7ccd0ba6d3c2520c4e
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-09 23:59:59 -0800

    Time: 11 ms (84.14%), Space: 13.6 MB (14.80%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,29 +5,28 @@ class Solution(object):
         :rtype: bool
         """
         checkList = []
-        openSymbol = ["(","[","{"]
-        closeSymbol = [")","]","}"]
+        openSymbol = ["(","{","["]
+        closeSymbol = [")","}","]"]
         
-        if len(s) % 2 != 0:
+        if len(s)%2 != 0:
             return False
+        
         
         for symbol in s:
             if symbol in openSymbol:
                 checkList.append(symbol)
             else:
                 if len(checkList) == 0:
-                    return False
-                elif checkList[-1] == "{" and symbol == "}":
+                    checkList.append(symbol)
+                elif symbol == "]" and checkList[-1] == "[":
                     checkList.pop()
-                elif checkList[-1] == "[" and symbol == "]":
+                elif symbol == "}" and checkList[-1] == "{":
                     checkList.pop()
-                elif checkList[-1] == "(" and symbol == ")":
+                elif symbol == ")" and checkList[-1] == "(":
                     checkList.pop()
-                elif symbol in closeSymbol:
+                else:
                     checkList.append(symbol)
         
         return not checkList
-            
                     
-                            
-        
+            
