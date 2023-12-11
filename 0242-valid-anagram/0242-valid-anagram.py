3e5c1c143658a0b6f49ea0537438fb433b060d7b
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-10 19:28:20 -0800

    Time: 40 ms (34.03%), Space: 14.7 MB (31.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,24 +5,27 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        s1 = []
-        s2 = []
         
-        for c1 in s:
-            s1.append(c1)
+        return sorted(s) == sorted(t)
     
-        for c2 in t:
-            s2.append(c2)
-
-        s1.sort()
-        s2.sort()
+#         s1 = []
+#         s2 = []
         
-        for i in range(len(t)):
-            if len(s1) != len(s2):
-                return False
-            elif s1[i] != s2[i]:
-                return False
-        return True
+#         for c1 in s:
+#             s1.append(c1)
+    
+#         for c2 in t:
+#             s2.append(c2)
+
+#         s1.sort()
+#         s2.sort()
+        
+#         for i in range(len(t)):
+#             if len(s1) != len(s2):
+#                 return False
+#             elif s1[i] != s2[i]:
+#                 return False
+#         return True
         
         
         
