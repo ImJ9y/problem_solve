70bff54946b52eb8e27c71b8eaf0cef8762d8b57
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-10 19:18:14 -0800

    Time: 39 ms (38.24%), Space: 16.1 MB (5.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -10,23 +10,19 @@ class Solution(object):
         
         for c1 in s:
             s1.append(c1)
-        
+    
         for c2 in t:
             s2.append(c2)
-        
+
         s1.sort()
         s2.sort()
         
         for i in range(len(t)):
             if len(s1) != len(s2):
                 return False
-            elif s1[len(s1)-1] != s2[len(s2)-1]:
-                return False
             elif s1[i] != s2[i]:
                 return False
-            
         return True
-        
         
         
         
