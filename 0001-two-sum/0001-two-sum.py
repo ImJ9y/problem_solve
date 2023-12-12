18ad13b1d219e3177d0a70e8a440f227335f6fde
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-12 15:11:20 -0800

    Time: 34 ms (90.09%), Space: 14.6 MB (5.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,24 +6,24 @@ class Solution(object):
         :rtype: List[int]
         """
         
-#         #HashMap - O(n)
-#         seen = {}
+        #HashMap - O(n)
+        seen = {}
         
-#         for i in range(len(nums)):
-#             diff = target - nums[i]
-            
-#             if diff in seen:
-#                 #find list value's index (Values()), index
-#                 return seen[diff], i
-#             else:
-#                 #list value : index
-#                 seen[nums[i]] = i
-        
-#         return seen.values()
-    
-        #O(n^2)
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return i,j
+            diff = target - nums[i]
+            
+            if diff in seen:
+                #find list value's index (Values()), index
+                return seen[diff], i
+            else:
+                #list value : index
+                seen[nums[i]] = i
+        
+        return seen.values()
+    
+        # #O(n^2)
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return i,j
                 
