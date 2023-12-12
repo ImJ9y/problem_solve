af8c75ba367367f8fc351c119e942d184f038f5b
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-12 15:09:41 -0800

    Time: 39 ms (74.87%), Space: 14.2 MB (56.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,11 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(0, len(nums)):
-            for j in range(1, len(nums)):
-                sum = nums[i] + nums[j]
-                if i == j:
-                    continue
-                
-                if sum == target:
-                    return i,j
+        seen = {}
+        
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            
+            if diff in seen:
+                #find list value's index (Values()), index
+                return seen[diff], i
+            else:
+                #list value : index
+                seen[nums[i]] = i
+        
+        return seen.values()
