be4726606717f7d319e338e588afc0c175321dca
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-12 19:28:52 -0800

    Time: 82 ms (23.57%), Space: 19.5 MB (14.88%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        #Using list as the default_factory, it is easy to group a sequence of key-value pairs into a dictionary of lists:
+        answer = defaultdict(list)
+        
+        for s in strs:
+            count = [0] * 26 #a...z (26 characters)
+            
+            for c in s:
+                #ord() - change to unicode character
+                #a = 80
+                #b = 81
+                #c = 82
+                count[ord(c)-ord('a')] += 1
+            
+            answer[tuple(count)].append(s)
+
+        return answer.values()
+        
+#         or
+
+#         answer = defaultdict(list)
+
+#         for s in strs:
+#             answer["".join(sorted(s))].append(s)
+
+#         return answer.values()
