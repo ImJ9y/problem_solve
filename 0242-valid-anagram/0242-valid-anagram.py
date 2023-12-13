83e81dc57586f7fe9a211f120b414f2f86d826ea
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-12 16:41:04 -0800

    Time: 25 ms (88.98%), Space: 15.3 MB (11.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -6,16 +6,32 @@ class Solution(object):
         :rtype: bool
         """
         
-        #return sorted(s) == sorted(t)
+#       return sorted(s) == sorted(t)
+        #or
+#       temp = []
+#       for c in s:
+#           temp.append(c)
         
-        temp = []
-        for c in s:
-            temp.append(c)
+#       for c2 in t:
+#           if c2 in temp:
+#               temp.remove(c2)
+#           else:
+#               temp.append(c2)
         
-        for c2 in t:
-            if c2 in temp:
-                temp.remove(c2)
-            else:
-                temp.append(c2)
+#       return not temp
         
-        return not temp
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+        
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i],0) #if hashmap doesn't have the value the default value = 0
+            countT[t[i]] = 1 + countT.get(t[i],0)
+        
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        
+        return True
+                
