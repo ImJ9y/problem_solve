c6048a4c55da8e53d321c4d0b45e85dfa6a73b78
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-12 16:31:09 -0800

    Time: 980 ms (5.01%), Space: 14.9 MB (28.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -6,5 +6,16 @@ class Solution(object):
         :rtype: bool
         """
         
-        return sorted(s) == sorted(t)
-    
+        #return sorted(s) == sorted(t)
+        
+        temp = []
+        for c in s:
+            temp.append(c)
+        
+        for c2 in t:
+            if c2 in temp:
+                temp.remove(c2)
+            else:
+                temp.append(c2)
+        
+        return not temp
