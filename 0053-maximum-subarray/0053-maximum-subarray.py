96ccb5f13d4c4a546c09dc368c470e5a786cd586
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-12 17:48:59 -0800

    Time: 530 ms (66.66%), Space: 26.3 MB (47.36%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -5,25 +5,12 @@ class Solution(object):
         :rtype: int
         """
         
+        #O(n) - Kadane’s Algorithm
         maxSum = nums[0]
+#       maxNum = float('-inf') #default value
         curSum = 0
         for num in nums:
             curSum = max(curSum, 0) + num
             maxSum = max(maxSum, curSum)
         
         return maxSum
-        
-        
-        
-        
-        
-        #maxNum = nums[0]
-#         maxNum = float('-inf') #default value
-#         curNum = 0
-        
-#         for num in nums:
-#             curNum = max(curNum, 0) + num
-#             #or curNum += num
-#             maxNum = max(maxNum, curNum)
-        
-#         return maxNum
