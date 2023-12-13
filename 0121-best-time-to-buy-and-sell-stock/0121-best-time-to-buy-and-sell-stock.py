98a36e8f3126258430a3c586c377dfe6a79549c3
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-12 16:06:39 -0800

    Time: 763 ms (66.41%), Space: 22.5 MB (36.65%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,19 +4,32 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        
+        #Two Pointers
         Left = 0
         Right = 1
         maxProfit = 0
-        
         while Right < len(prices):
-            if prices[Left] > prices[Right]:
+            if prices[Left] < prices[Right]:
+                profit = prices[Right] - prices[Left]
+                maxProfit = max(maxProfit, profit)
+            else:
                 Left = Right
-            
-            curProfit = prices[Right] - prices[Left]
-            if curProfit > maxProfit:
-                maxProfit = curProfit
-    
             Right += 1
-        
         return maxProfit
+        
+        
+#         while Right < len(prices):
+#             if prices[Left] > prices[Right]:
+#                 Left = Right
+            
+#             curProfit = prices[Right] - prices[Left]
+#             if curProfit > maxProfit:
+#                 maxProfit = curProfit
+    
+#             Right += 1
+        
+#         return maxProfit
+        
+        
         
