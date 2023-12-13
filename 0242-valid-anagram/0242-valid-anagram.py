95412690138b2459869550b93c866fa89c5dc2e1
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-12 16:28:49 -0800

    Time: 45 ms (17.91%), Space: 14.5 MB (43.10%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -8,28 +8,3 @@ class Solution(object):
         
         return sorted(s) == sorted(t)
     
-#         s1 = []
-#         s2 = []
-        
-#         for c1 in s:
-#             s1.append(c1)
-    
-#         for c2 in t:
-#             s2.append(c2)
-
-#         s1.sort()
-#         s2.sort()
-        
-#         for i in range(len(t)):
-#             if len(s1) != len(s2):
-#                 return False
-#             elif s1[i] != s2[i]:
-#                 return False
-#         return True
-        
-        
-        
-        
-                     
-        
-        
