2d91934c91c978443572feb511360eec0f62bbc4
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-12 17:38:02 -0800

    Time: 41 ms (30.75%), Space: 15.3 MB (7.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,24 +5,24 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        if len(s) != len(t):
+             return False
         
         countS, countT = {}, {}
         
-        if len(s) != len(t):
-            return False
-        
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i],0)
+            countS[s[i]] = 1 + countS.get(s[i],0) #if hashmap doesn't have the value the default value = 0
             countT[t[i]] = 1 + countT.get(t[i],0)
         
-        for k in countS:
-            if countS[k] != countT.get(k,0):
+        for c in countS:
+            #order can be different so we need to use .get() for countT
+            if countS[c] != countT.get(c, 0):
                 return False
         
         return True
-        
-        
-        
+
+        #or
+    
 #       return sorted(s) == sorted(t)
         
     
@@ -41,19 +41,5 @@ class Solution(object):
         
 #       return not temp
         
-#         if len(s) != len(t):
-#             return False
-        
-#         countS, countT = {}, {}
-        
-#         for i in range(len(s)):
-#             countS[s[i]] = 1 + countS.get(s[i],0) #if hashmap doesn't have the value the default value = 0
-#             countT[t[i]] = 1 + countT.get(t[i],0)
-        
-#         for c in countS:
-#             #order can be different so we need to use .get() for countT
-#             if countS[c] != countT.get(c, 0):
-#                 return False
-        
-#         return True
+
                 
