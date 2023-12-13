1c8a553d9d1989bbeed62dba04c2d521904b3067
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-12 19:09:01 -0800

    Time: 108 ms (24.79%), Space: 18.2 MB (41.75%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        
+        intervals.sort()
+        merged = []
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+        
+        return merged
+        
