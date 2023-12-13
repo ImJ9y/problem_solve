aec757b537650e1c64509336e1e745852431cef4
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2023-12-12 23:08:28 -0800

    Time: 25 ms (88.13%), Space: 13.4 MB (38.38%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
@@ -4,6 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        nums.sort()
+        x = nums[-1]
+        y = nums[-2]
+        return (x - 1) * (y - 1)
+
+
 #         maxMul = 0
 #         for i in range(len(nums)):
 #             curMul = 0
@@ -13,14 +19,14 @@ class Solution(object):
         
 #         return maxMul
         
-        biggest = 0
-        second_biggest = 0
-        for num in nums:
-            if num > biggest:
-                second_biggest = biggest
-                biggest = num
-            else:
-                second_biggest = max(second_biggest, num)
         
-        return (biggest - 1) * (second_biggest - 1)
+#         biggest = 0
+#         second_biggest = 0
+#         for num in nums:
+#             if num > biggest:
+#                 second_biggest = biggest
+#                 biggest = num
+#             else:
+#                 second_biggest = max(second_biggest, num)
         
+#         return (biggest - 1) * (second_biggest - 1)
