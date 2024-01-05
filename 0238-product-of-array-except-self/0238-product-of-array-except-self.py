a6a7333299109a2afd91be9d4697308a1452970a
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-01-05 15:18:04 -0800

    Time: 163 ms (75.30%), Space: 20.8 MB (55.74%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,14 +6,14 @@ class Solution(object):
         """
         length = len(nums)
         answer = [1] * length
-
+        
         for i in range(1, length):
             answer[i] = nums[i-1] * answer[i-1]
         
-        Right = nums[-1] #generating the last value of Nums List
+        R = nums[-1]
         
-        for i in range(length-2, -1,-1):
-            answer[i] *= Right #Multiple the number from list to Answer
-            Right *= nums[i] #Next number from the list
-            
+        for i in range(length-2, -1 ,-1):
+            answer[i] *= R
+            R *= nums[i]
+        
         return answer
