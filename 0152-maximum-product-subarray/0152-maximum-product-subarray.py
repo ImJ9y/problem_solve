772c6a2ba3b95c868f8e9c02826345e95da4b8c6
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-01-28 14:29:55 -0800

    Time: 57 ms (30.94%), Space: 11.8 MB (99.15%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -4,23 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        maxNum = max(nums)
-        currMin, currMax = 1,1
         
+        currMin, currMax = 1, 1
+        maxNum = max(nums)
+        
+        if len(nums) == 1:
+            return nums[0]
         
         for num in nums:
-            #0 is netural -> need to refresh the min and max
-            
             if num == 0:
-                currMin, currMax = 1,1
-
-            #when we use num*currMax in currMin - the value is changing
-            #so we are going to generate new object to generate previous num*currMax
+                currMin, currMax = 1, 1
+                
             temp = num * currMax
             currMax = max(num, num*currMin, num*currMax)
             currMin = min(num, num*currMin, temp)
-            
+        
             maxNum = max(maxNum, currMax)
         
         return maxNum
-            
