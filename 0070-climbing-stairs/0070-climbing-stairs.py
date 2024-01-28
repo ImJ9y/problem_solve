3104787e58f45cd4c5ed4e20aef5e4aece8c4920
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-01-28 15:32:34 -0800

    Time: 13 ms (61.66%), Space: 11.6 MB (96.91%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+         #Brute Force
+        one, two = 1,1
+        
+        for i in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
+            
+        return one
