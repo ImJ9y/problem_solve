09cf0156b2556b542729cb71b17121262ed5547a
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-02-18 17:44:24 -0800

    Time: 742 ms (85.02%), Space: 14.7 MB (97.87%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,38 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        #To arrange smallest to largest
-        nums.sort()
-        answer = []
         
-		#Need to add Three values
-        for i in range(len(nums)-2):
-			#if index i and i-1 are duplicated, move to next
-            if i > 0 and nums[i] == nums[i-1]:
+        ans = []
+        nums.sort()
+        
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i-1]:
                 continue
-            L = i + 1
-            R = len(nums) - 1
+                
+            L, R = i+1, len(nums)-1
             
             while L < R:
-                total = nums[i] + nums[L] + nums[R]
-                if total < 0:
-                    #Because we sorted it earlier so Left side is smallest
-                    #Need to make sum larger
-                    L += 1
-                elif total > 0:
-                    #Need to make sum smaller
+                threeSum = a + nums[L] + nums[R]
+                if threeSum > 0:
                     R -= 1
+                elif threeSum < 0:
+                    L += 1
                 else:
-                    #when total == 0
-                    triplet = [nums[i],nums[L],nums[R]]
-                    answer.append(triplet)
-                    #Check duplicate
-                    while L < R and nums[L] == triplet[1]:
+                    ans.append((a, nums[L], nums[R]))
+                    L += 1
+                    while nums[L] == nums[L-1] and L < R:
                         L += 1
-                    while L < R and nums[R] == triplet[2]:
-                        R -= 1
             
-        return answer
-    
+        return ans
                     
-        
+                    
