d99ba80732fa3ada645288042fd1d7779cf02b6e
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-03-01 23:42:49 -0800

    Time: 17 ms (26.80%), Space: 11.7 MB (68.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -5,27 +5,11 @@ class Solution(object):
         :rtype: int
         """
         
-#          #Brute Force
-#         one, two = 1,1
+        one, two = 1,1
         
-#         for i in range(n-1):
-#             temp = one
-#             one = one + two
-#             two = temp
-            
-#         return one
-
-        if n == 1:
-            return 1
+        for i in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
         
-        if n == 2:
-            return 2
-
-        memo = [0] * (n + 1)
-        memo[1] = 1
-        memo[2] = 2
-        
-        for i in range(3, n + 1):
-            memo[i] = memo[i - 1] + memo[i - 2]
-        
-        return memo[n]
+        return one
