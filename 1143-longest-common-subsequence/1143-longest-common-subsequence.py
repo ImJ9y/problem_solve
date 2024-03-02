e838f180592c3656a26491165c7a77729ae02e54
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-03-02 15:28:06 -0800

    Time: 542 ms (55.69%), Space: 34 MB (35.01%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -5,11 +5,10 @@ class Solution(object):
         :type text2: str
         :rtype: int
         """
-        #2D
-        dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1) + 1)]
+        dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
         
-        for i in range(len(text1) -1, -1 ,-1):
-            for j in range(len(text2) -1, -1 ,-1):
+        for i in range(len(text1)-1,-1,-1):
+            for j in range(len(text2)-1, -1, -1):
                 if text1[i] == text2[j]:
                     dp[i][j] = 1 + dp[i+1][j+1]
                 else:
