7f8584c8311c552abcad96471640f753fb9b3ac9
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-03-02 15:05:11 -0800

    Time: 743 ms (69.69%), Space: 12.1 MB (92.16%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -5,13 +5,13 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
-        dp = [amount+1] * (amount+1)
         
+        dp = [amount + 1] * (amount + 1)
         dp[0] = 0
         
         for a in range(1, amount + 1):
             for c in coins:
                 if a - c >= 0:
-                    dp[a] = min(dp[a], 1+dp[a-c])
+                    dp[a] = min(dp[a], 1 + dp[a-c])
         
         return dp[amount] if dp[amount] != amount + 1 else -1
