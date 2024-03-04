c00750fe1fb2283dce599b4175a1aa224aa5947e
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-03-03 17:55:12 -0800

    Time: 2086 ms (35.33%), Space: 12.1 MB (76.35%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -4,9 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        
         LIS = [1] * len(nums)
+        
         for i in range(len(nums)-1,-1,-1):
-            for j in range(i + 1, len(nums)):
+            for j in range(i+1, len(nums)):
                 if nums[i] < nums[j]:
                     LIS[i] = max(LIS[i], 1+LIS[j])
         
