70268187669f744b8f01dc9776a8f6d2c8be3dd7
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-03-05 18:51:16 -0800

    Time: 12 ms (94.81%), Space: 11.7 MB (86.43%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        dp = [False] * (len(s)+1)
+        dp[len(s)] = True
+        
+        for i in range(len(s)-1,-1,-1):
+            for word in wordDict:
+                if (i+len(word)) <= len(s) and s[i:i+len(word)] == word:
+                    dp[i] = dp[i+len(word)]
+                
+                if dp[i]:
+                    break
+        
+        return dp[0]
