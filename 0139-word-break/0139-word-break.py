313684ccab36393a19e633efc00384d9436481b8
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-03-06 18:34:16 -0800

    Time: 23 ms (45.43%), Space: 11.9 MB (67.50%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -11,11 +11,10 @@ class Solution(object):
         
         for i in range(len(s)-1,-1,-1):
             for word in wordDict:
-                if (i + len(word)) <= len(s) and s[i:i+len(word)] == word:
+                if (i+len(word)) <= len(s) and s[i:i+len(word)]== word:
                     dp[i] = dp[i+len(word)]
                 
                 if dp[i]:
                     break
         
         return dp[0]
-        
