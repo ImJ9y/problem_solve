d6d732604f8b3ece5fdcafe29af0a990fe023600
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-03-07 19:08:59 -0800

    Time: 353 ms (72.00%), Space: 12.5 MB (95.12%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        goal = len(nums)-1
+        
+        for i in range(len(nums)-1,-1,-1):
+            if i + nums[i] >= goal:
+                goal = i
+        
+        return goal == 0
