50372e57a72f506fb25396750a2e7e2cd161d59b
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-03-07 19:02:02 -0800

    Time: 16 ms (47.68%), Space: 11.7 MB (60.61%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        row = [1] * (n + 1)
+        
+        for i in range(m-1):
+            newRow = [1] * n
+            for j in range(n-2,-1,-1):
+                newRow[j] = row[j] + newRow[1+j]
+            
+            row = newRow
+        
+        return row[0]
