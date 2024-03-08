0cf88560f03833e3ed6ed5f18a31d98a3722fb8e
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-03-07 19:02:32 -0800

    Time: 16 ms (47.68%), Space: 11.7 MB (60.61%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -10,7 +10,8 @@ class Solution(object):
         for i in range(m-1):
             newRow = [1] * n
             for j in range(n-2,-1,-1):
-                newRow[j] = row[j] + newRow[1+j]
+                newRow[j] = newRow[j+1] + row[j]
+                
             
             row = newRow
         
