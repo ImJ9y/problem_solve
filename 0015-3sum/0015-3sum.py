f572b9064fde0fbffd74ade103b9b7429a596498
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-03-10 17:47:48 -0700

    Time: 767 ms (78.70%), Space: 14.7 MB (98.29%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,25 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
         ans = []
         nums.sort()
         
         for i, a in enumerate(nums):
-            if i > 0 and nums[i-1] == a:
+            if i > 0 and nums[i-1] == a: 
                 continue
             L, R = i+1, len(nums)-1
+            
             while L < R:
-                threeSum = a + nums[L] + nums[R]
-                if threeSum < 0:
+                threeSome = a + nums[L] + nums[R]
+                if threeSome < 0:
                     L += 1
-                elif threeSum > 0:
-                    R -=1
+                elif threeSome > 0:
+                    R -= 1
                 else:
                     ans.append((a, nums[L], nums[R]))
                     L += 1
                     while nums[L] == nums[L-1] and L < R:
                         L += 1
-        
+
         return ans
                 
