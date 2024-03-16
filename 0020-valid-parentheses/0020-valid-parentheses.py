e60a67cd9b4e5036655a8ce9dd91f4a285850623
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-03-16 11:13:06 -0700

    Time: 16 ms (48.94%), Space: 11.9 MB (32.23%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,34 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-#        temp = []
-#        openSymbol = ["(","{","["]
-#        closeSymbol = [")","}","]"]
         
-#         if len(s)%2 != 0:
-#             return False
-        
-#         for symbol in s:
-#             if symbol in openSymbol:
-#                 temp.append(symbol)
-#             else:
-#                 if len(temp) == 0:
-#                     temp.append(symbol)
-#                 elif symbol == "]" and temp[-1] == "[":
-#                     temp.pop()
-#                 elif symbol == "}" and temp[-1] == "{":
-#                     temp.pop()
-#                 elif symbol == ")" and temp[-1] == "(":
-#                     temp.pop()
-#                 else:
-#                     temp.append(symbol)
-        
-#         return not temp
-
-
-#        or
-
-        #HashMap
         answer = []
         closeToOpen = {")":"(", "}":"{", "]":"["}
         
@@ -45,3 +18,7 @@ class Solution(object):
                 answer.append(symbol)
         
         return not answer
+                     
+        
+        
+        
