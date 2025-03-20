0e59c69b0b3af0ff4559a7aa17605d5e247011dc
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-03-20 10:49:19 -0700

    Time: 77 ms (23.55%), Space: 15.3 MB (44.74%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -4,6 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+
+        # First, identify the distinct values and remove extra duplicates.
+        # k = 1
+        # for i in range(1, len(nums)):
+        #     if nums[i-1] != nums[i]:
+        #         nums[k] = nums[i]
+        #         k += 1
+        
+        # print(nums)
+        # return k
+
+        # Next, allow up to two occurrences of each element in the list.
         k = 1
         count = 1
         for i in range(1, len(nums)):
@@ -17,7 +29,6 @@ class Solution(object):
                 k += 1
         
         return k
-
 
 
         
