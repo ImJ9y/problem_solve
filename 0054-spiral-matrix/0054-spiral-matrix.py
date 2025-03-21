d283248066f04f7e387b66199cb5c5ebf97bbaec
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-03-21 13:17:08 -0700

    Time: 0 ms (100%), Space: 12.5 MB (12.24%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -4,32 +4,30 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        output = []
         LEFT, RIGHT = 0, len(matrix[0])
         TOP, BOTTOM = 0, len(matrix)
 
-        while LEFT < RIGHT and TOP < BOTTOM:
-            #get every i in the top row
-            for i in range(LEFT, RIGHT):
-                output.append(matrix[TOP][i])
-            TOP += 1
+        ans = []
 
-            #get every i in the right col
+        while LEFT < RIGHT and TOP < BOTTOM:
+            for i in range(LEFT, RIGHT):
+                ans.append(matrix[TOP][i])
+            TOP += 1
+            
+
             for i in range(TOP, BOTTOM):
-                output.append(matrix[i][RIGHT - 1])
+                ans.append(matrix[i][RIGHT-1])
             RIGHT -= 1
 
-            if not(LEFT < RIGHT and TOP < BOTTOM):
+            if not(LEFT<RIGHT and TOP < BOTTOM):
                 break
 
-            #get every i in the bottom row
             for i in range(RIGHT-1, LEFT-1, -1):
-                output.append(matrix[BOTTOM -1][i])
+                ans.append(matrix[BOTTOM-1][i])
             BOTTOM -= 1
 
-            #get every i in the left col
             for i in range(BOTTOM-1, TOP-1, -1):
-                output.append(matrix[i][LEFT])
+                ans.append(matrix[i][LEFT])
             LEFT += 1
         
-        return output
+        return ans
