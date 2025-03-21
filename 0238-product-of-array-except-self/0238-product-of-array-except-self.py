c49ef55995fb48a997272de99445a101087cb5fe
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-03-21 12:15:34 -0700

    Time: 31 ms (68.93%), Space: 20.2 MB (82.99%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,15 +5,14 @@ class Solution(object):
         :rtype: List[int]
         """
         ans = [1] * len(nums)
-        
+
         for i in range(1, len(nums)):
             ans[i] = nums[i-1] * ans[i-1]
-        
+
         R = nums[-1]
-        
-        for j in range(len(nums)-2,-1,-1):
-            ans[j] = ans[j] * R
-            R = nums[j] * R
+
+        for i in range(len(nums)-2,-1,-1):
+            ans[i] *= R
+            R *= nums[i]
         
         return ans
-            
