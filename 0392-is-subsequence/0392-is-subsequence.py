578bcfba729b06d6624046a21eaebf9e63b91f01
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-03-31 20:45:19 -0700

    Time: 21 ms (5.3%), Space: 12.9 MB (11.95%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        S = len(s)
+        T = len(t)
+        j = 0
+        for i in range(T):
+            if j < S and s[j] == t[i]:
+                j += 1
+        
+        return S == j
