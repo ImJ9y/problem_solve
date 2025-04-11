eff227a84d4023c7a287fb7b5eec48f6ee36f7cc
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-04-10 18:25:39 -0700

    Time: 4 ms (24.91%), Space: 12.5 MB (57.61%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -9,12 +9,13 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        cur = head 
+        
+        cur = head
 
         while cur and cur.next:
             if cur.val == cur.next.val:
                 cur.next = cur.next.next
             else:
                 cur = cur.next
-        
+
         return head
