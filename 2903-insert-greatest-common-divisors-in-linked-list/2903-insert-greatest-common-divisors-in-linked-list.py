0782fdb94d288a48305b837ca0f974ddbf7af7cd
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-04-11 16:43:27 -0700

    Time: 39 ms (46.26%), Space: 19.5 MB (72.24%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.py
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.py
@@ -3,29 +3,30 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution(object):
-    def gcd(self, a, b):
-            while b:
-                a, b = b, a%b
-            
-            return a
+    def helper(self, a, b):
+        while b:
+            a,b = b,a%b
+        
+        return a
     
     def insertGreatestCommonDivisors(self, head):
         """
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        
         cur = head
 
         while cur and cur.next:
-            greatcommon = self.gcd(cur.val, cur.next.val)
+            greatestCommon = self.helper(cur.val, cur.next.val)
 
-            new_head = ListNode(greatcommon)
+            temp = cur.next
+            
+            new_head = ListNode(greatestCommon)
             new_head.next = cur.next
             cur.next = new_head
 
-            cur = new_head.next
+            cur = temp
         
         return head
+            
