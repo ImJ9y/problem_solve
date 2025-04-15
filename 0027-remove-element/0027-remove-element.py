ac614d506331d886f011131569124f60ee3a369f
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-04-14 20:57:37 -0700

    Time: 0 ms (100%), Space: 12.5 MB (51.68%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -6,10 +6,11 @@ class Solution(object):
         :rtype: int
         """
         k = 0
+        
         for i in range(len(nums)):
             if nums[i] != val:
                 nums[k] = nums[i]
                 k += 1
-                
+    
         return k
-        
+                
