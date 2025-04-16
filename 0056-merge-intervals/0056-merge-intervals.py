d4d2081defb4a72c12324ee37567ef3be759fc19
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-04-16 16:50:13 -0700

    Time: 5 ms (99.38%), Space: 15.8 MB (96.35%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,14 +4,14 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
+        
         ans = []
         intervals.sort()
 
         for interval in intervals:
-            if not ans or ans[-1][1] < interval[0]:
-                ans.append(interval)
-            else:
+            if ans and ans[-1][1] >= interval[0]:
                 ans[-1][1] = max(ans[-1][1], interval[1])
+            else:
+                ans.append(interval)
         
         return ans
-        
