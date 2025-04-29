3cead80d8a74217c452333967a144c683261d50e
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-04-29 15:07:09 -0700

    Time: 12 ms (16.57%), Space: 13.6 MB (95.28%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -4,26 +4,26 @@ class Solution(object):
         :type tokens: List[str]
         :rtype: int
         """
-        
-        if len(tokens) == 1:
-            return int(tokens[0])
-
         stack = []
-        
+
         for token in tokens:
-            if token in '+-*/':
+
+            if stack and token in "+-*/":
                 b = stack.pop()
                 a = stack.pop()
+
                 if token == '+':
                     stack.append(a + b)
                 elif token == '-':
                     stack.append(a - b)
                 elif token == '*':
                     stack.append(a * b)
-                elif token == '/' and b != 0:
-                    # Integer division toward zero
-                    stack.append(int(float(a) / b))
+                elif token == '/':
+                    stack.append(int(float(a)/b))
             else:
                 stack.append(int(token))
 
+        
+
         return stack[-1]
+        
