531bd17ff954f7c5090e69d7a5e80a5eafe9dad0
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-04-29 16:57:56 -0700

    Time: 0 ms (100%), Space: 12.5 MB (18.04%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -9,7 +9,7 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        slow, fast = head, head
+        slow, fast = head,head
 
         while fast and fast.next:
             slow = slow.next
