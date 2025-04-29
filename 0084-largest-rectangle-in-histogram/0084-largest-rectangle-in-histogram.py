0f603a6715b9d09c6a23d0e2339f4eef2767656f
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-04-28 20:01:11 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -4,19 +4,20 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
-        stack = [] #(index, height)
         max_area = 0
+        stack = [] #[i, height]
 
         for i, height in enumerate(heights):
             start = i
             while stack and stack[-1][1] > height:
-                stackIndex, stackHeight = stack.pop()
-                max_area = max(max_area, stackHeight * (i - stackIndex))
-                start = stackIndex
-            
-            stack.append((start, height))
-        
+                stackindex, stackheight = stack.pop()
+                max_area = max(max_area, stackheight * (i-stackindex))
+                start = stackindex
+
+            stack.append([i,height])
+
         for i, height in stack:
             max_area = max(max_area, height * (len(heights)-i))
+
         
         return max_area
