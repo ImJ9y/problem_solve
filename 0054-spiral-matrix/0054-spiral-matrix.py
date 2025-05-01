f3f6780708ef456d46535820d697de6e381c2c71
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-05-01 14:47:16 -0700

    Time: 0 ms (100%), Space: 12.4 MB (82.54%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -4,38 +4,30 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        LEFT, RIGHT = 0, len(matrix[0])
         TOP, BOTTOM = 0, len(matrix)
+        LEFT, RIGHT = 0, len(matrix[0])
 
         ans = []
 
-        while LEFT < RIGHT and TOP < BOTTOM:
-            #LEFT to RIGHT
+        while TOP < BOTTOM and LEFT < RIGHT:
             for i in range(LEFT, RIGHT):
                 ans.append(matrix[TOP][i])
             TOP += 1
 
-            #TOP to BOTTOM
             for i in range(TOP, BOTTOM):
                 ans.append(matrix[i][RIGHT-1])
             RIGHT -= 1
-
-            if not (LEFT < RIGHT and TOP < BOTTOM):
+            
+            if not (TOP < BOTTOM and LEFT < RIGHT):
                 break
 
-            #RIGHT to LEFT
-            for i in range(RIGHT-1,LEFT-1,-1):
+            for i in range(RIGHT-1,LEFT,-1):
                 ans.append(matrix[BOTTOM-1][i])
             BOTTOM -= 1
 
-            #BOTTOM to TOP
-            for i in range(BOTTOM-1,TOP-1,-1):
+
+            for i in range(BOTTOM, TOP-1, -1):
                 ans.append(matrix[i][LEFT])
-            
             LEFT += 1
-
-
+            
         return ans
-
-
-
