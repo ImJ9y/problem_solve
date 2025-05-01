ccd7b89a71918c6d35fb156df7a354c0d8f26eba
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-04-30 20:37:32 -0700

    Time: 0 ms (100%), Space: 12.5 MB (12.12%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -4,21 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[str]
         """
-        
         ans = []
         i = 0
-
         while i < len(nums):
-            start = nums[i]
-
+            start = i
             while i < len(nums)-1 and nums[i]+1 == nums[i+1]:
                 i += 1
             
-            if start < nums[i]:
-                ans.append(str(start) + "->" + str(nums[i]))
+            if nums[start] < nums[i]:
+                ans.append(str(nums[start]) + "->" + str(nums[i]))
             else:
-                ans.append(str(start))
+                ans.append(str(nums[start]))
             
             i += 1
         
         return ans
+
