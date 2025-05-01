a161e0bb7a9df1eef438368f4d8b3ec51002cfc6
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-04-30 19:14:54 -0700

    Time: 0 ms (100%), Space: 12.4 MB (55.47%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -10,18 +10,36 @@ class Solution(object):
         :type n: int
         :rtype: Optional[ListNode]
         """
-        dummy_list = ListNode(0, head)
-        left = dummy_list
-        right = head
+        # dummy_list = ListNode(0, head)
+        # left = dummy_list
+        # right = head
 
-        while n > 0 and right:
-            n -= 1
-            right = right.next
+        # while n > 0 and right:
+        #     n -= 1
+        #     right = right.next
         
-        while right:
-            left = left.next
-            right = right.next
+        # while right:
+        #     left = left.next
+        #     right = right.next
         
-        left.next = left.next.next
+        # left.next = left.next.next
 
-        return dummy_list.next
+        # return dummy_list.next
+
+        cur = head
+        count = 0
+
+        while cur:
+            count += 1
+            cur = cur.next
+        
+        if count - n == 0:
+            return head.next
+
+        cur = head
+        for i in range(count - n - 1):
+            cur = cur.next
+        
+        cur.next = cur.next.next
+
+        return head
