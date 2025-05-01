9ba075bd66410cbc16823db579f485fbe69f568c
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-04-30 21:12:14 -0700

    Time: 0 ms (100%), Space: 12.5 MB (53.44%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,8 +7,7 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        
-        length = m + n - 1
+        length = (m + n) - 1
 
         while m > 0 and n > 0:
             if nums1[m-1] < nums2[n-1]:
@@ -17,11 +16,21 @@ class Solution(object):
             else:
                 nums1[length] = nums1[m-1]
                 m -= 1
-
+            
             length -= 1
         
+
         while n > 0:
             nums1[length] = nums2[n-1]
             n -= 1
             length -= 1
-    
+
+        
+
+
+        
+            
+
+        
+
+         
