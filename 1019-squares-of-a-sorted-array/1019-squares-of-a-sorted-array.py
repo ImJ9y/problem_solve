574fa5ca9039437d76a30b54a3f302b790ead437
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-05-07 21:47:23 -0700

    Time: 18 ms (23.31%), Space: 14.4 MB (38.88%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
@@ -5,7 +5,8 @@ class Solution(object):
         :rtype: List[int]
         """
         res = []
-        L, R = 0, len(nums) - 1
+        L, R = 0, len(nums)-1
+
         while L <= R:
             if nums[L] * nums[L] < nums[R] * nums[R]:
                 res.append(nums[R] * nums[R])
@@ -13,19 +14,5 @@ class Solution(object):
             else:
                 res.append(nums[L] * nums[L])
                 L += 1
-        
+
         return res[::-1]
-
-
-        # new_array = []
-
-        # for num in nums:
-        #     new_array.append(num * num)
-
-        # for i in range(1, len(new_array)):
-        #     j = i
-        #     while j > 0 and new_array[j-1] > new_array[j]:
-        #         new_array[j-1], new_array[j] = new_array[j], new_array[j-1]
-        #         j -= 1
-        
-        # return new_array
