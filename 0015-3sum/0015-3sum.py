b6b53de5966c5b32f1ff5d82be7d239ca753dfb7
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-05-08 17:59:44 -0700

    Time: 707 ms (68.21%), Space: 18.4 MB (40.98%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -7,22 +7,28 @@ class Solution(object):
         ans = []
         nums.sort()
         
-        for i, a in enumerate(nums):
-            if i > 0 and nums[i-1] == a: 
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            L, R = i+1, len(nums)-1
             
+            L = i + 1
+            R = len(nums)-1
             while L < R:
-                threeSome = a + nums[L] + nums[R]
-                if threeSome < 0:
+                cur = nums[i] + nums[L] + nums[R]
+                if cur < 0:
                     L += 1
-                elif threeSome > 0:
+                elif cur > 0:
                     R -= 1
                 else:
-                    ans.append((a, nums[L], nums[R]))
-                    L += 1
-                    while nums[L] == nums[L-1] and L < R:
+                    ans.append([nums[i], nums[L], nums[R]])
+
+                    while L<R and nums[L] == nums[L + 1]:
                         L += 1
+                    while L <R and nums[R] == nums[R - 1]:
+                        R -= 1 
+                    
+                    # Move both pointers once more
+                    L += 1
+                    R -= 1
 
         return ans
-                
