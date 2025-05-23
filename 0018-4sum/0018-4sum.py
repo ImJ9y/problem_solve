83819b57c077729b2939414bbdfe29079029ab9f
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-05-23 14:09:39 -0700

    Time: 495 ms (82%), Space: 12.5 MB (57.09%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -5,17 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        ans = []
         nums.sort()
+        ans = []
 
         for i in range(len(nums)-3):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-
             for j in range(i+1, len(nums)-2):
                 if j > i+1 and nums[j] == nums[j-1]:
                     continue
-
+                
                 L = j + 1
                 R = len(nums)-1
 
@@ -27,13 +26,14 @@ class Solution(object):
                     elif cur > target:
                         R -= 1
                     else:
-                        ans.append([nums[i], nums[j], nums[L], nums[R]])
+                        ans.append([nums[i],nums[j],nums[L],nums[R]])
+
                         while L < R and nums[L] == nums[L+1]:
                             L += 1
-                        while L < R and nums[R] == nums[R-1]:
+                        while L < R and nums[R]==nums[R-1]:
                             R -= 1
-                    
+                        
                         L += 1
                         R -= 1
-
+            
         return ans
