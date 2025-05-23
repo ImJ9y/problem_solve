d528dabc2983c6bfad69050fbfac6de191218634
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-05-23 14:02:26 -0700

    Time: 379 ms (93.64%), Space: 12.4 MB (95.15%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -6,10 +6,12 @@ class Solution(object):
         :rtype: int
         """
         nums.sort()
-        close_sum = nums[0] + nums[1] + nums[2]
-        min_distance = abs(target - close_sum)
 
-        for i in range(len(nums)):
+        close_sum = nums[0] + nums[1] + nums[2]
+        close_distance = abs(target - close_sum)
+        
+
+        for i in range(len(nums)-1):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             
@@ -20,15 +22,15 @@ class Solution(object):
                 cur_sum = nums[i] + nums[L] + nums[R]
                 cur_distance = abs(target - cur_sum)
 
-                if cur_distance < min_distance:
-                    close_sum = cur_sum
-                    min_distance = cur_distance
-                
                 if cur_sum == target:
-                    return cur_sum
-                elif cur_sum < target:
+                    return target
+                elif close_distance > cur_distance:
+                    close_distance = cur_distance
+                    close_sum = cur_sum
+
+                if cur_sum < target:
                     L += 1
                 else:
                     R -= 1
-        
+                
         return close_sum
