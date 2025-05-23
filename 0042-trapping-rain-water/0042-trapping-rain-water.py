d76f541cab138ac8d5e3695f0f2c0d54e89a5857
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-05-22 22:18:53 -0700

    Time: 8 ms (82.96%), Space: 13.6 MB (64.97%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,24 +4,27 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        l_wall, r_wall = 0, 0
-        n = len(height)
-        max_left = [0] * n
-        max_right = [0] * n
+        if not height:
+            return 0
 
-        for i in range(n):
-            j = -i -1
-            max_left[i] = l_wall
-            max_right[j] = r_wall
+        left, right = 0, len(height)-1
+        max_left, max_right = 0, 0
+        trapped_water = 0
 
-            l_wall = max(l_wall, height[i])
-            r_wall = max(r_wall, height[j])
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= max_left:
+                    max_left = height[left]
+                else:
+                    trapped_water += max_left - height[left]
+
+                left += 1
+            else:
+                if height[right] >= max_right:
+                    max_right = height[right]
+                else:
+                    trapped_water += max_right - height[right]
+                
+                right -= 1
         
-        ans = [0] * n
-
-        for i in range(n):
-            possible = min(max_left[i], max_right[i])
-            ans[i] = max(0, possible-height[i])
-        
-        return sum(ans)
-            
+        return trapped_water
