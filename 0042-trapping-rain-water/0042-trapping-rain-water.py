ae728fff8ba4b6b2ef26c322b19ea878dd11ea41
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-05-26 11:02:17 -0700

    Time: 36 ms (15.55%), Space: 14.3 MB (15.3%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,49 +4,21 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        l_wall, r_wall = 0, 0
-        n = len(height)
-        max_left = [0] * n
-        max_right = [0] * n
+        l_wall, r_wall = 0,0
+        max_l, max_r = [0] * len(height), [0] * len(height)
 
-        for i in range(n):
-            j = -i -1
-            max_left[i] = l_wall
-            max_right[j] = r_wall
+        for i in range(len(height)):
+            j = -i-1
+
+            max_l[i] = l_wall
+            max_r[j] = r_wall
 
             l_wall = max(l_wall, height[i])
             r_wall = max(r_wall, height[j])
         
-        ans = [0] * n
+        ans = [0] * len(height)
 
-        for i in range(n):
-            possible = min(max_left[i], max_right[i])
-            ans[i] = max(0, possible-height[i])
-        
-        return sum(ans)
-
-        # if not height:
-        #     return 0
-
-        # left, right = 0, len(height)-1
-        # max_left, max_right = 0, 0
-        # trapped_water = 0
-
-        # while left < right:
-        #     if height[left] < height[right]:
-        #         if height[left] >= max_left:
-        #             max_left = height[left]
-        #         else:
-        #             trapped_water += max_left - height[left]
-
-        #         left += 1
-        #     else:
-        #         if height[right] >= max_right:
-        #             max_right = height[right]
-        #         else:
-        #             trapped_water += max_right - height[right]
-                
-        #         right -= 1
-        
-        # return trapped_water
+        for i in range(len(height)):
+            ans[i] += max(0, min(max_l[i], max_r[i]) - height[i])
             
+        return sum(ans)
