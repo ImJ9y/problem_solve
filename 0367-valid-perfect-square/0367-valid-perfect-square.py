8297ed1d6d8db04c35698f8af71ad668e799f313
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-05-28 15:19:19 -0700

    Time: 0 ms (100%), Space: 12.4 MB (80.51%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -4,7 +4,21 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-        root = int((num**.5))
+        # root = int((num**.5))
 
-        return True if (root * root) == num else False
+        # return True if (root * root) == num else False
+
+        L, R = 1, num
+        while L <= R:
+            M = (L+R)//2
+
+            if M * M > num:
+                R = M - 1
+            elif M * M < num:
+                L = M + 1
+            else:
+                return True
+        
+        return False
+
         
