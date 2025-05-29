373a004247a177a6f306ceb8fe40999944e78465
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-05-28 19:31:58 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -5,23 +5,22 @@ class Solution(object):
         :type h: int
         :rtype: int
         """
-        L, R = ceil(sum(piles) / h), max(piles)
+        L, R = ceil(sum(piles)/h), max(piles)
         res = R
 
-        while L <= R:
+        while L < R:
             M = (L+R)//2
-            if M == 0:
-                break
-                
+
             hours = 0
+
             for p in piles:
-                hours += ceil((p/M))
+                hours += ceil(p/M)
 
             if hours <= h:
                 res = min(res, M)
-                R = M -1
+                R -= 1
             else:
-                L = M + 1
+                L += 1
 
-        return int(res)
+        return int(res)            
 
