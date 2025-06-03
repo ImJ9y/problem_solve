5cb5153af25eaf9e5e62edfd6e41986e4e93317d
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-06-03 16:55:54 -0700

    Time: 16 ms (60.17%), Space: 12.5 MB (12.55%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -5,14 +5,13 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        ans = ""
-
         length = max(len(word1), len(word2))
-
+        ans = ""
+        
         for i in range(length):
             if i < len(word1):
                 ans += word1[i]
             if i < len(word2):
                 ans += word2[i]
-    
+        
         return ans
