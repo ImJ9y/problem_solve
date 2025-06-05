9afdc691a0bb364f93d1d945efb7d559f75e339e
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-06-05 14:43:48 -0700

    Time: 1 ms (25.23%), Space: 12.5 MB (52.71%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,15 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        res = [0,0,0]
+        # ans = [0,0,0]
 
-        for num in nums:
-            res[num] += 1
+        # for num in nums:
+        #     ans[num] += 1
         
-        RED = res[0]
-        WHITE = res[1]
-        BLUE = res[2]
+        # RED = ans[0]
+        # WHITE = ans[1]
+        # BLUE = ans[2]
+
+        # nums[:RED] = [0] * RED
+        # nums[RED:RED+WHITE] = [1] * WHITE
+        # nums[RED+WHITE:] = [2] * BLUE
+
+
+        for i in range(1, len(nums)):
+            j = i
+            while j > 0 and nums[j-1] > nums[j]:
+                nums[j], nums[j-1] = nums[j-1], nums[j]
+                j -= 1
         
-        nums[:RED] = [0] * RED
-        nums[RED:RED+WHITE] = [1] * WHITE
-        nums[WHITE+RED:] = [2] * BLUE
