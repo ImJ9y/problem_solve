36425e9b529485eddfe9be33c7de237b8e52df4e
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-06-05 17:29:45 -0700

    Time: 39 ms (14.84%), Space: 12.5 MB (82.24%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -5,28 +5,24 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
-
+        
         if numRows == 1:
             return s
-
-        ans = [''] * numRows
+        
         row = 0
-        i = 0
-
+        ans = [""] * numRows
+        i = 1
         for c in s:
-            row += i
-
+            ans[row] += c
             if row == numRows-1:
                 i = -1
             if row == 0:
                 i = 1
             
-            ans[row] += c
-
-        result = ""
-
-        for s in ans:
-            result += "".join(s)
+            row += i
         
-        return result
-            
+        res = ""
+        for new_s in ans:
+            res += "".join(new_s)
+    
+        return res
