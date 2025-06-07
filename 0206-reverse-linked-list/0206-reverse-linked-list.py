cd9715a6e664c8186a036759be1487ff4a4773bc
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-06-07 14:00:43 -0700

    Time: 0 ms (100%), Space: 14.5 MB (52.59%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -10,7 +10,7 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         cur, prev = head, None
-
+        
         while cur:
             temp = cur.next
             cur.next = prev
@@ -18,3 +18,4 @@ class Solution(object):
             cur = temp
         
         return prev
+            
