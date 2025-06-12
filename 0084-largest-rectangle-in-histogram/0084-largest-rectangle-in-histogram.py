2364665c31bd33909f05c6d100ae7ebb98b0ffed
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-06-11 20:14:24 -0700

    Time: 231 ms (31.09%), Space: 30.9 MB (5.76%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -10,12 +10,13 @@ class Solution(object):
         for i, height in enumerate(heights):
             start = i
             while stack and stack[-1][1] > height:
-                stackIndex, stackHeight = stack.pop()
-                max_area = max(max_area, stackHeight * (i - stackIndex))
-                start = stackIndex
+                stk_index, stk_height = stack.pop()
+                max_area = max(stk_height * (i - stk_index), max_area)
+                start = stk_index
             stack.append([start, height])
         
-        for i,height in stack:
-            max_area = max(max_area, height * (len(heights) -i))
-        
+        for i, height in stack:
+            max_area = max(max_area, (height * (len(heights)-i)))
+
         return max_area
+        
