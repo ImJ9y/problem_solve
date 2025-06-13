2db03a8c51a7e6a50962b823ae4da4a765fecf2f
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-06-13 11:10:33 -0700

    Time: 5 ms (26.24%), Space: 19.8 MB (92.99%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        L, R = 0, len(s)-1
+        L, R = 0,len(s)-1
 
-        while L <= R:
-            s[L], s[R] = s[R], s[L]
+        while L < R:
+            s[L],s[R] = s[R],s[L]
             L += 1
             R -= 1
         
