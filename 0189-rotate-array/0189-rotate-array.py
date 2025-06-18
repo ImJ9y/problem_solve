433bee4da836e54e16201cb6b16c609d4a37c6e5
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-06-18 10:28:17 -0700

    Time: 67 ms (5.02%), Space: 26 MB (8.86%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        new_nums = collections.deque(nums)
+
+        for i in range(k):
+            temp = new_nums.pop()
+            new_nums.appendleft(temp)
+        
+        for i in range(len(new_nums)):
+            nums[i] = new_nums[i]
