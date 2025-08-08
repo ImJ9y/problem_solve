649edc2601e117c984536673a6904e51e5a19186
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-08-08 00:01:32 -0700

    Time: 15 ms (29.1%), Space: 14.7 MB (6.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,16 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        ans = {None:None}
-        check = len(nums)//2
+        result = {None:None}
 
-        for num in nums:
-            if num in ans:
-                ans[num] += 1
+        for i in range(len(nums)):
+            if nums[i] in result:
+                result[nums[i]] += 1
             else:
-                ans[num] = 1
-            
-        for key, val in ans.items():
-            if val > check:
-                return key
+                result[nums[i]] = 1
         
+        max_num = 0
+        ans = 0
+        for key, val in result.items():
+            if max_num < val:
+                max_num = val
+                ans = key
+
+        return ans
