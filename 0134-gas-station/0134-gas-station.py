c22cc993d2d5bcc10e009853e93bd8f71a771c87
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-08-13 23:40:26 -0700

    Time: 21 ms (87.93%), Space: 18.1 MB (33.66%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -5,11 +5,13 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
+        
         if sum(gas) < sum(cost):
             return -1
-        
+
         cur = 0
         start = 0
+
         for i in range(len(gas)):
             cur += gas[i] - cost[i]
 
@@ -18,33 +20,4 @@ class Solution(object):
                 start = i + 1
         
         return start
-
-        # n = len(gas)
-
-        # if n == 1:
-        #     if gas[0] > cost[0]:
-        #         return 0
-        #     else:
-        #         return -1
         
-        # for i in range(n):
-        #     cur = 0
-        #     for j in range(n + 1):
-                
-        #         if (i + j) > n-1 :
-        #             k = i + j - n 
-        #         else: 
-        #             k = i + j
-                
-        #         cur = cur + gas[k]
-
-        #         if cur < cost[k]:
-        #             break
-        #         else:
-        #             cur = cur - cost[k]
-        #         if cur > 0 and j == n:
-        #             return i   
-        # return -1
-
-
-
