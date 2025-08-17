fcf1cc4e1b5f4a2b7341fdf1e45283c97677681d
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-08-17 00:59:11 -0700

    Time: 71 ms (60.93%), Space: 19 MB (90.44%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        current = prices[0]
+        cur = prices[0]
         total = 0
 
         for price in prices:
-            if current < price:
-                total = max(price - current, total)
+            if cur < price:
+                total = max(total, price - cur)
             else:
-                current = price 
-        
+                cur = price
+
         return total
