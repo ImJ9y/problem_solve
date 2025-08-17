a3c991955cb0a2d23b4b25788652878ae5c69585
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-08-17 00:37:39 -0700

    Time: 4 ms (45.39%), Space: 13.8 MB (58.18%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -6,9 +6,9 @@ class Solution(object):
         """
         k = 1
 
-        for i in range(len(nums)-1):
-            if nums[i] != nums[i+1]:
-                nums[k] = nums[i+1]
+        for i in range(1, len(nums)):
+            if nums[i-1] != nums[i]:
+                nums[k] = nums[i]
                 k += 1
         
         return k
