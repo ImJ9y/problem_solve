246df54ea09389523395cdc64f2080969be371e1
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-08-17 00:42:59 -0700

    Time: 69 ms (65.12%), Space: 15.4 MB (16.61%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -4,17 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        k = 1
         count = 1
+        k = 1
 
         for i in range(1, len(nums)):
-            if(nums[i-1] == nums[i]):
+            if nums[i-1] == nums[i]:
                 count += 1
             else:
                 count = 1
-            
-            if count <= 2:
+
+            if count < 3:
                 nums[k] = nums[i]
                 k += 1
         
         return k
+        
