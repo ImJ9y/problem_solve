ad1f49cf4c2fd9aec723b32c9e0ba356dee894c7
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-03 15:44:02 -0700

    Time: 19 ms (78.87%), Space: 13.4 MB (25.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -7,7 +7,11 @@ class Solution(object):
         goal = len(nums)-1
 
         for i in range(len(nums)-1,-1,-1):
+            #i = your current position.
+            #nums[i] = how far you can jump forward from there.
+            #So i + nums[i] = the furthest index you can reach if you jump from i.
             farthest = i + nums[i]
+            
 
             if farthest >= goal:
                 goal = i
