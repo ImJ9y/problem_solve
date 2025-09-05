db472c9274915721c8fc3a02b4c2049b6103bc6a
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-05 14:40:44 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -4,20 +4,19 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
-        pages = [0] * (len(citations)+1)
+        papers = [0] * (len(citations)+1)
 
         for citation in citations:
             if citation >= len(citations):
-                pages[-1] += 1
+                papers[-1] += 1
             else:
-                pages[citation] += 1
+                papers[citation] += 1
         
-
-        paper = pages[-1]
         h = len(citations)-1
+        paper = papers[-1]
 
         while h > paper:
             h -= 1
-            paper += pages[h]
-        
+            paper += papers[h]
+    
         return h
