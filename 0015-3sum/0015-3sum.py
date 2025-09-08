00d571043ac09be88521b27d0d250ca0a0a3d819
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-08 10:43:18 -0700

    Time: 747 ms (41.05%), Space: 19 MB (21.62%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,24 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        ans = []
+        
+        result = []
         nums.sort()
+
+        print(nums)
 
         for i in range(len(nums)-1):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            
-            L, R = i+1, len(nums)-1
+
+            L = i + 1
+            R = len(nums)-1
 
             while L < R:
                 cur = nums[i] + nums[L] + nums[R]
 
-                if cur < 0:
-                    L += 1
-                elif cur > 0:
+                if cur > 0:
                     R -= 1
+                elif cur < 0:
+                    L += 1
                 else:
-                    ans.append([nums[i], nums[L], nums[R]])
+                    result.append([nums[i], nums[L], nums[R]])
 
                     while L < len(nums)-1 and nums[L] == nums[L+1]:
                         L += 1
@@ -31,5 +35,6 @@ class Solution(object):
                     
                     L += 1
                     R -= 1
-                
-        return ans
+            
+        return result
+        
