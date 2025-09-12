af8e054b2048faf2a1c2043828a3aff22b9d9014
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-12 14:05:30 -0700

    Time: 4 ms (64%), Space: 22.5 MB (63.86%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -6,14 +6,22 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
 
-        # dp = collections.deque(nums)
-
         # for i in range(k):
-        #     temp = dp.pop()
-        #     dp.appendleft(temp)
+        #     temp = nums.pop()
+        #     nums.insert(0, temp)
+        
+        # if k == 0:
+        #     return
+        
+        # dq = collections.deque(nums)
+        
+        # for i in range(k):
+        #     dq.appendleft(dq[-1])
+        #     dq.pop()
+        
+        # for i in range(len(dq)):
+        #     nums[i] = dq[i]
 
-        # for i in range(len(nums)):
-        #     nums[i] = dp[i]
+        k = k % len(nums) #look for the reminder of nums length
 
-        k = k%len(nums)
         nums[:] = nums[-k:] + nums[:-k]
