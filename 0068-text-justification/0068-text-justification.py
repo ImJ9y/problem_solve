a8a2a94e3828c858c82630b308f77e8264458f89
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-16 13:46:57 -0700

    Time: 0 ms (100%), Space: 12.5 MB (52.75%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -12,36 +12,38 @@ class Solution(object):
         while i < len(words):
             cur_word = words[i]
 
+            #if line is not completed:
             if cur_len + len(cur_word) <= maxWidth:
                 cur_line.append(cur_word)
-                cur_len += len(cur_word) + 1 #adding space
+                cur_len += len(cur_word) + 1 #with space
                 i += 1
-            else:
-                #reset our process
-                spaces = maxWidth - cur_len + len(cur_line) #total spaces left
 
+            #if line is completed
+            else:
+                spaces = maxWidth - cur_len + len(cur_line)
                 added = 0
                 j = 0
-                
-                while added < spaces: #while we still need to add space
+
+                while added < spaces:
                     if j >= len(cur_line)-1:
                         j = 0
-                    
+                        
                     cur_line[j] += " "
                     added += 1
                     j += 1
+                
                 output.append("".join(cur_line))
                 cur_line, cur_len = [], 0
         
 
-        #Handling last line
-        last_line = " ".join(cur_line)
-        trail_space = maxWidth - len(last_line)
-        last_line += " " * trail_space
+        last_word = " ".join(cur_line)
+        extra_spaces = maxWidth - len(last_word)
+        last_word += " " * extra_spaces
 
-        output.append(last_line)
+        output.append(last_word)
 
         return output
+
 
 
 
