606b5247da2c154b464050168417b377b66decd0
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-16 00:13:44 -0700

    Time: 0 ms (100%), Space: 12.5 MB (52.66%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -6,39 +6,41 @@ class Solution(object):
         :rtype: List[str]
         """
         output = []
-        cur_line, cur_length = [], 0
-
+        cur_line, cur_len = [], 0
         i = 0
 
         while i < len(words):
-            
-            #Line complete
-            if cur_length + len(cur_line) + len(words[i]) > maxWidth:       
-                extra_space = maxWidth - cur_length
-                spaces = extra_space // max(1, len(cur_line)-1)
-                remainder = extra_space % max(1, len(cur_line)-1)
+            cur_word = words[i]
 
-                for j in range(max(1, len(cur_line)-1)):
-                    cur_line[j] += " " * spaces
-                    if remainder != 0:
-                        cur_line[j] += " "
-                        remainder -= 1
+            if cur_len + len(cur_word) <= maxWidth:
+                cur_line.append(cur_word)
+                cur_len += len(cur_word) + 1 #adding space
+                i += 1
+            else:
+                #reset our process
+                spaces = maxWidth - cur_len + len(cur_line) #total spaces left
+
+                added = 0
+                j = 0
                 
+                while added < spaces: #while we still need to add space
+                    if j >= len(cur_line)-1:
+                        j = 0
+                    
+                    cur_line[j] += " "
+                    added += 1
+                    j += 1
                 output.append("".join(cur_line))
+                cur_line, cur_len = [], 0
+        
 
-                #reeset the line and length
-                cur_line, cur_length = [], 0
-
-
-            cur_line.append(words[i])
-            cur_length += len(words[i])
-            i += 1
-
-        #Handling last line
-        last_line = " ".join(cur_line)
-        trail_space = maxWidth - len(last_line)
-        last_line += " " * trail_space
-
-        output.append(last_line)
+        for word in range(len(cur_line)-1):
+            cur_line[word] += " "
+        
+        cur_line[-1] += " " * (maxWidth - cur_len + 1)
+        output.append("".join(cur_line))
 
         return output
+
+
+
