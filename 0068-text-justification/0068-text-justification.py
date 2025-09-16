dc693f70b0932c0e4af00f551ff02a062c6cb6d0
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-16 14:10:09 -0700

    Time: 0 ms (100%), Space: 12.3 MB (85.27%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -12,30 +12,28 @@ class Solution(object):
         while i < len(words):
             cur_word = words[i]
 
-            #if line is not completed:
+            #not complete
             if cur_len + len(cur_word) <= maxWidth:
                 cur_line.append(cur_word)
-                cur_len += len(cur_word) + 1 #with space
+                cur_len += len(cur_word) + 1 #space
                 i += 1
-
-            #if line is completed
+            #complete
             else:
                 spaces = maxWidth - cur_len + len(cur_line)
+                j = 0 
                 added = 0
-                j = 0
 
                 while added < spaces:
                     if j >= len(cur_line)-1:
                         j = 0
-                        
+                    
                     cur_line[j] += " "
-                    added += 1
                     j += 1
+                    added += 1
                 
                 output.append("".join(cur_line))
                 cur_line, cur_len = [], 0
         
-
         last_word = " ".join(cur_line)
         extra_spaces = maxWidth - len(last_word)
         last_word += " " * extra_spaces
@@ -43,7 +41,3 @@ class Solution(object):
         output.append(last_word)
 
         return output
-
-
-
-
