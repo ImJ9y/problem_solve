d079c2e5d8bb0bef5f16200b35c2ee2c73d097fc
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-19 13:58:18 -0700

    Time: 30 ms (69.32%), Space: 20.2 MB (84.76%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        result = [1] * len(nums)
-
-        for i in range(1, len(nums)):
-            result[i] = nums[i-1] * result[i-1]
+        output = [1] * len(nums)
         
+        for i in range(1, len(nums)):
+            output[i] = nums[i-1] * output[i-1]
+
         R = nums[-1]
 
-        for i in range(len(nums)-2,-1,-1):
-            result[i] *= R
+        for i in range(len(nums)-2, -1,-1):
+            output[i] *= R
             R *= nums[i]
-
-        return result
+        
+        return output
