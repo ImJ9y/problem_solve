3f2650de25b16d3d9c34645e82685292f3aafe90
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-18 22:32:05 -0700

    Time: 21 ms (86.02%), Space: 18.1 MB (33.19%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -7,14 +7,17 @@ class Solution(object):
         """
         if sum(gas) < sum(cost):
             return -1
-
+        
         start = 0
         cur = 0
 
         for i in range(len(gas)):
-            cur += gas[i] - cost[i]
             if cur < 0:
                 cur = 0
-                start = i + 1
+                start = i
+            
+            cur += gas[i] - cost[i]
         
         return start
+
+
