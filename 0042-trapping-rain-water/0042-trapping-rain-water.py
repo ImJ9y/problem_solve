ddf150ffdb31a68f7a0d9a87ad3128ed84e5e037
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-21 13:12:28 -0700

    Time: 8 ms (78.28%), Space: 13.6 MB (63.27%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -4,22 +4,22 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        max_l, max_r = 0, 0
         L, R = 0, len(height)-1
+        max_l, max_r = 0, 0
         trapped_water = 0
 
         while L < R:
             if height[L] < height[R]:
-                if max_l <= height[L]:
+                if max_l < height[L]:
                     max_l = height[L]
                 else:
-                    trapped_water += min(max_l, max_l - height[L])
+                    trapped_water += max_l - height[L]
                 L += 1
             else:
-                if max_r <= height[R]:
+                if max_r < height[R]:
                     max_r = height[R]
                 else:
-                    trapped_water += min(max_r, max_r - height[R])
+                    trapped_water += max_r - height[R]
                 R -= 1
-        
+            
         return trapped_water
