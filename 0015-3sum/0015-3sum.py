116eca5667416a585f27276d22b93047df331cc0
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-22 22:32:04 -0700

    Time: 724 ms (59.11%), Space: 18.3 MB (88.44%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,34 +5,34 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         
-        result = []
+        output = []
         nums.sort()
 
-        for i in range(len(nums)-1):
+
+        for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-
+            
             L = i + 1
             R = len(nums)-1
 
             while L < R:
                 cur = nums[i] + nums[L] + nums[R]
 
-                if cur > 0:
-                    R -= 1
-                elif cur < 0:
+                if cur < 0:
                     L += 1
+                elif cur > 0:
+                    R -= 1
                 else:
-                    result.append([nums[i], nums[L], nums[R]])
+                    output.append([nums[i], nums[L], nums[R]])
 
                     while L < len(nums)-1 and nums[L] == nums[L+1]:
                         L += 1
                     
                     while R > 0 and nums[R] == nums[R-1]:
                         R -= 1
-                    
+            
                     L += 1
                     R -= 1
-            
-        return result
-        
+    
+        return output
