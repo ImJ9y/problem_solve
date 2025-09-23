515fe3e1ec067d640b2b7fa8cb98eedbac3b201c
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-23 15:05:46 -0700

    Time: 134 ms (23.84%), Space: 13.2 MB (53.86%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,19 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        
         new_s = ""
-
         for c in s:
             if c.isalnum():
                 new_s += c.lower()
         
-        L, R = 0, len(new_s)-1
-
-        while L < R:
-            if new_s[L] == new_s[R]:
-                L += 1
-                R -= 1
-            else:
-                return False
-        
-        return True
+        return new_s == new_s[::-1]
