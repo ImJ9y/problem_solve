aba6d4833985f5e76103a2e23659cfd29230487e
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-24 14:12:44 -0700

    Time: 9215 ms (6%), Space: 13.2 MB (53.61%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -6,27 +6,27 @@ class Solution(object):
         :rtype: List[int]
         """
         if not s or not words: return []
-
+        
         word_freq = {}
 
         for word in words:
             word_freq[word] = 1 + word_freq.get(word, 0)
-
+        
         len_word = len(words[0])
-        len_window = len(words) * len_word
+        len_window = len_word * len(words)
 
-        output = []   
+        output = []
 
-        for i in range(len(s) - len_window + 1):
+        for i in range(len(s)-len_window+1):
             j = i
             sub_freq = {}
 
             while j < len_window + i:
                 cur_c = s[j:j+len_word]
 
-                if cur_c not in word_freq:
+                if cur_c not in words:
                     break
-
+            
                 sub_freq[cur_c] = 1 + sub_freq.get(cur_c, 0)
 
                 if sub_freq[cur_c] > word_freq[cur_c]:
@@ -38,3 +38,4 @@ class Solution(object):
                 output.append(i)
         
         return output
+
