2a54fbd2bbe53d5861a2688dfc3aa50474207719
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-25 22:13:24 -0700

    Time: 4 ms (97.46%), Space: 12.6 MB (97.04%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -4,12 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
-        dp = [1] * len(nums)
+        # dp = [1] * len(nums)
 
-        for i in range(len(nums)-1,-1,-1):
-            for j in range(i +1 , len(nums)):
-                if nums[i] < nums[j]:
-                    dp[i] = max(dp[i], 1 + dp[j])
+        # for i in range(len(nums)-1,-1,-1):
+        #     for j in range(i +1 , len(nums)):
+        #         if nums[i] < nums[j]:
+        #             dp[i] = max(dp[i], 1 + dp[j])
         
-        return max(dp)
+        # return max(dp)
+
+        output = []
+
+        for num in nums:
+            if len(output) == 0 or output[-1] < num:
+                output.append(num)
+            else:
+                index = bisect_left(output, num)
+                output[index] = num
+            
+        
+        return len(output)
