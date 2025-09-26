1698e43b7d3c5c97ebd1f9c656d3c5b4a0f67df8
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-25 22:28:54 -0700

    Time: 819 ms (71.69%), Space: 13 MB (79.93%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -5,18 +5,14 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
-        #dp with 0 ~ amount + 1 list
-        #try to find remain each amount index - coin(s)
-        # if remain is >= 0 then we are going to find dp to calculate
         
         dp = [amount + 1] * (amount + 1)
         dp[0] = 0
-
-        for i in range(amount+1):
+    
+        for i in range(amount + 1):
             for coin in coins:
                 remain = i - coin
-
                 if remain >= 0:
-                    dp[i] =  min(dp[i], 1 + dp[remain])
-
-        return dp[amount] if dp[amount] != amount + 1 else -1
+                    dp[i] = min(dp[i], 1 + dp[remain])
+        
+        return dp[amount] if dp[amount] != amount + 1 else -1 #larger than any possible number of coins needed
