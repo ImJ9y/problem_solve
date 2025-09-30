d72e5d2116c832b23ee45099eb62b8803c7c4d22
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-09-30 01:45:30 -0700

    Time: 11 ms (89.24%), Space: 12.5 MB (54.02%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -5,13 +5,15 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        length = max(len(word1), len(word2))
+        total_len = max(len(word1), len(word2))
         ans = ""
-        
-        for i in range(length):
+
+        for i in range(total_len):
             if i < len(word1):
                 ans += word1[i]
             if i < len(word2):
                 ans += word2[i]
-        
+
         return ans
+
+
