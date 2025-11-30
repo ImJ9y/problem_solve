44afbb3ee5d31843a951f28f36289a0a99ec5bfe
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2025-11-30 02:21:11 -0800

    Time: 731 ms (72.1%), Space: 18.2 MB (56.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,18 +1,16 @@
-class Solution(object):
-    def coinChange(self, coins, amount):
-        """
-        :type coins: List[int]
-        :type amount: int
-        :rtype: int
-        """
-        
-        dp = [amount + 1] * (amount + 1)
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount+1] * (amount + 1)
+
         dp[0] = 0
-    
-        for i in range(amount + 1):
+
+        for i in range(len(dp)):
             for coin in coins:
                 remain = i - coin
-                if remain >= 0:
-                    dp[i] = min(dp[i], 1 + dp[remain])
+
+                if remain < 0:
+                    continue
+                
+                dp[i] = min(dp[i], 1 + dp[remain])
         
-        return dp[amount] if dp[amount] != amount + 1 else -1 #larger than any possible number of coins needed
+        return dp[amount] if dp[amount] != amount+1 else -1
