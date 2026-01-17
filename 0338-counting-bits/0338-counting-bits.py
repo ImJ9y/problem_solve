467ec599103e8798ba55226cfa400d4def0965a7
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-01-17 11:02:47 -0800

    Time: 53 ms (17.38%), Space: 20.5 MB (5.87%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,17 +1,16 @@
-class Solution(object):
-    def countBits(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = []
+
+        def countbit(n):
+            count = 0
+            while n > 0:
+                count += n & 1
+                n >>= 1
+            
+            return count
         
-        #n log(n)
-        ans = [0] * (n + 1)
-        offset = 1
-        
-        for i in range(1, n+1):
-            if offset * 2 == i:
-                offset = i
-            ans[i] = 1 + ans[i - offset]
+        for i in range(n+1):
+            ans.append(countbit(i))
         
         return ans
