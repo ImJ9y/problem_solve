d457c3e0199bb69041939ca7e2e2e86eb0874ca7
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-01-19 02:18:07 -0800

    Time: 0 ms (100%), Space: 14 MB (51.6%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,12 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        seen = []
-        
+        # Because every number except one appears exactly twice, the duplicate numbers will eventually pair up and "cancel" each other out to become 0.Example walkthrough with nums = [2, 2, 1]:Start: res = 0First 2: 0 ^ 2 = 2Second 2: 2 ^ 2 = 0 (The pair cancels out)The 1: 0 ^ 1 = 1Result: 1 (The single number remains)This approach is ideal for your goal of becoming a software engineer because it meets the strict interview constraints: it runs in linear time ($O(n)$) and uses constant extra space ($O(1)$).
+
+        ans = 0
         for num in nums:
-            if num in seen:
-                seen.remove(num)
-            else:
-                seen.append(num)
+            ans ^= num
         
-        return seen[0]
+        return ans
+
+        # seen = []
+
+        # for num in nums:
+        #     if num in seen:
+        #         seen.remove(num)
+        #     else:
+        #         seen.append(num)
+        
+        # return seen[0]
