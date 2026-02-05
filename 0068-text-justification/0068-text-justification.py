038053c8e5bc84ef8b3914e3127af8936ba27b75
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-02-04 16:07:16 -0800

    Time: 0 ms (100%), Space: 19.4 MB (45.95%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -6,12 +6,11 @@ class Solution:
 
         while i < len(words):
             if length + len(line) + len(words[i]) > maxWidth:
-
                 extra_spaces = maxWidth - length
                 spaces = extra_spaces // max(1,len(line)-1)
                 remainder = extra_spaces % max(1,len(line)-1)
 
-                for j in range(max(1, len(line)-1)):
+                for j in range(max(1,len(line)-1)):
                     line[j] += ' ' * spaces
                     if remainder:
                         line[j] += ' '
@@ -19,12 +18,14 @@ class Solution:
                 
                 res.append("".join(line))
                 line, length = [], 0
-
+            
+            #complete line
             line.append(words[i])
             length += len(words[i])
             i += 1
         
         last_line = " ".join(line)
-        trail_spaces = maxWidth - len(last_line)
-        res.append(last_line + ' ' * trail_spaces)
+        last_spaces = maxWidth - len(last_line)
+        res.append(last_line + ' ' *last_spaces)
+
         return res
