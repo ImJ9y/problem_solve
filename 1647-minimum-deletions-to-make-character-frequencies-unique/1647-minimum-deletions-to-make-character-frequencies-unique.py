ac3e41544196ba1251f4cbcaf0149b9d01ec7e8c
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-02-05 12:29:19 -0800

    Time: 143 ms (11.96%), Space: 20.3 MB (39.61%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -3,15 +3,17 @@ class Solution:
         char_map = {}
         count = 0
 
-        for c in s:
-            char_map[c] = 1 + char_map.get(c, 0)
+        for i in range(len(s)):
+            char_map[s[i]] = 1 + char_map.get(s[i], 0)
         
-        used_freq = set()
+        
+        unique = set()
         for key, val in char_map.items():
-            while val > 0 and val in used_freq:
+            while val > 0 and val in unique:
                 val -= 1
                 count += 1
-            
-            used_freq.add(val) #unique or 0
-            
+    
+            unique.add(val)
+        
         return count
+            
