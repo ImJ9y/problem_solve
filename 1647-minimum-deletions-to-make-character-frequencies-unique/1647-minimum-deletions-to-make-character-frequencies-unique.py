978a931a5145360a2c56bf4abb3bfaf2b4fe4c53
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-02-04 19:54:23 -0800

    Time: 131 ms (20.91%), Space: 20.5 MB (11.05%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,14 +1,12 @@
 class Solution:
     def minDeletions(self, s: str) -> int:
         char_map = {}
-        used_freq = set()
         count = 0
 
         for c in s:
             char_map[c] = 1 + char_map.get(c, 0)
         
         used_freq = set()
-        
         for key, val in char_map.items():
             while val > 0 and val in used_freq:
                 val -= 1
