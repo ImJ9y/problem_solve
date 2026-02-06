448689bace4efe067ff74efc6bfc68e26e00b4cb
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-02-05 20:32:37 -0800

    Time: 0 ms (100%), Space: 19.3 MB (51.67%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -3,9 +3,9 @@ class Solution:
         L, R = 0, len(nums)-1
         ans = -1
 
+
         while L <= R:
             M = (L+R)//2
-
             if nums[M] == target:
                 ans = M
                 break
@@ -16,9 +16,9 @@ class Solution:
                 else:
                     L = M + 1
             else:
-                if nums[M] < target <= nums[R]:
+                if nums[R] >= target > nums[M]:
                     L = M + 1
                 else:
                     R = M - 1
-        
+
         return ans
