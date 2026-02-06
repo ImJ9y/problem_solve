3f37326671cbc5fc5ac44a2200757a7de3476cd2
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-02-05 21:00:14 -0800

    Time: 10 ms (71.1%), Space: 23.7 MB (6.51%) - LeetHub

--- 0298-binary-tree-longest-consecutive-sequence/0298-binary-tree-longest-consecutive-sequence.py
+++ 0298-binary-tree-longest-consecutive-sequence/0298-binary-tree-longest-consecutive-sequence.py
@@ -7,17 +7,20 @@
 class Solution:
     def longestConsecutive(self, root: Optional[TreeNode]) -> int:
         self.longestpath = 0
-        def dfs(cur, prev, length):
-            if cur:
-                if cur.val -1 == prev:
-                    length += 1
-                    self.longestpath = max(self.longestpath, length)
 
-                    dfs(cur.left, cur.val, length)
-                    dfs(cur.right, cur.val, length)
-                else:
-                    dfs(cur.left, cur.val, 1)
-                    dfs(cur.right, cur.val, 1)
-        
+        def dfs(root, prev, length):
+            if not root:
+                return
+            
+            if root.val-1 == prev:
+                length += 1
+                self.longestpath = max(self.longestpath, length)
+
+                dfs(root.left, root.val, length)
+                dfs(root.right, root.val, length)
+            else:
+                dfs(root.left, root.val, 1)
+                dfs(root.right, root.val, 1)
+            
         dfs(root, root.val-1, 0)
         return self.longestpath
