9a5628e1e0a713b9d8891dbc213c550946d3e8b5
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-02-05 21:18:50 -0800

    Time: 150 ms (10%), Space: 20.3 MB (24.31%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -6,14 +6,15 @@ class Solution:
         for i in range(len(s)):
             char_map[s[i]] = 1 + char_map.get(s[i], 0)
         
-        
         unique = set()
         for key, val in char_map.items():
             while val > 0 and val in unique:
                 val -= 1
                 count += 1
-    
+            
             unique.add(val)
         
         return count
+
+
             
