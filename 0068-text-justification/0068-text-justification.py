54939d056919a0f008d7f6150a2e1f9f64835eb9
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-02-08 10:00:15 -0800

    Time: 0 ms (100%), Space: 19.5 MB (32.89%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -1,29 +1,29 @@
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        lines, length = [], 0
         res = []
-        line, length = [], 0
         i = 0
 
         while i < len(words):
-            if length + len(line) + len(words[i]) > maxWidth:
+            if length + len(lines) + len(words[i]) > maxWidth:
                 extra_spaces = maxWidth - length
-                spaces = extra_spaces//max(1, len(line)-1)
-                remainder = extra_spaces%max(1, len(line)-1)
+                spaces = extra_spaces // max(1, len(lines)-1)
+                remainder = extra_spaces % max(1, len(lines)-1)
 
-                for j in range(max(1, len(line)-1)):
-                    line[j] += ' ' * spaces
+                for j in range(max(1,len(lines)-1)):
+                    lines[j] += ' ' * spaces
                     if remainder:
-                        line[j] += ' '
+                        lines[j] += ' '
                         remainder -= 1
                 
-                res.append("".join(line))
-                line, length = [], 0
+                res.append("".join(lines))
+                lines, length = [], 0
 
-            line.append(words[i])
+            lines.append(words[i])
             length += len(words[i])
             i += 1
         
-        last_line = " ".join(line)
+        last_line = " ".join(lines)
         last_spaces = maxWidth - len(last_line)
         res.append(last_line + ' ' * last_spaces)
         return res
