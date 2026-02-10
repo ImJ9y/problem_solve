d753cef1242abaa9774d7312ce780c6fe87cea45
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-02-09 21:13:31 -0800

    Time: 3 ms (36.39%), Space: 19.3 MB (57.46%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,19 +1,18 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        closed_symbol = {')':'(', '}':'{',']':'['}
-        result = []
+class Solution:
+    def isValid(self, s: str) -> bool:
+        closed_p = {')':'(','}':'{',']':'['}
+        res = []
 
-        for symbol in s:
-            if symbol in closed_symbol:
-                if result and closed_symbol[symbol] == result[-1]:
-                    result.pop()
+        for i in range(len(s)):
+            if s[i] in closed_p:
+                print(closed_p[s[i]])
+                if res and res[-1] == closed_p[s[i]]:
+                    res.pop()
                 else:
                     return False
             else:
-                result.append(symbol)
-        
-        return not result
+                res.append(s[i])
+
+        return not res
+            
+
