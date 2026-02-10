0bd5d3905f96e8032539aa55750dc49152e67951
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-02-09 21:13:55 -0800

    Time: 3 ms (36.39%), Space: 19.4 MB (42.32%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,7 +5,6 @@ class Solution:
 
         for i in range(len(s)):
             if s[i] in closed_p:
-                print(closed_p[s[i]])
                 if res and res[-1] == closed_p[s[i]]:
                     res.pop()
                 else:
