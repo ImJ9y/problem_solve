34daf79a17e9ae6f2178c883e79be3b76f84c2ce
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-02-09 21:23:04 -0800

    Time: 16 ms (23.48%), Space: 20.5 MB (5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        char_map1 = {}
-        char_map2 = {}
+        # char_map1 = {}
+        # char_map2 = {}
 
-        for c in s:
-            char_map1[c] = 1 + char_map1.get(c, 0)
+        # for c in s:
+        #     char_map1[c] = 1 + char_map1.get(c, 0)
         
-        for c in t:
-            char_map2[c] = 1 + char_map2.get(c, 0)
+        # for c in t:
+        #     char_map2[c] = 1 + char_map2.get(c, 0)
 
-        return char_map1 == char_map2
+        # return char_map1 == char_map2
+
+        return sorted(s) == sorted(t)
