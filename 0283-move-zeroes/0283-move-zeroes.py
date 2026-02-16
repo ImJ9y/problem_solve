a2c495e9c9916da65d079ad4ee9e171f1768541f
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-02-16 14:04:13 -0500

    Time: 8 ms (25.88%), Space: 20.4 MB (70.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,6 +4,7 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         k = 0
+
         for i in range(len(nums)):
             if nums[i] != 0:
                 nums[k], nums[i] = nums[i], nums[k]
