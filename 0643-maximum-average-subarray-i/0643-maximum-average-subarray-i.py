07bbb5d19f4b97a53e408fb2d2810787f2cabb22
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-02-16 17:43:48 -0500

    Time: 56 ms (78.06%), Space: 28.9 MB (71.25%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -3,9 +3,11 @@ class Solution:
         sum_num = sum(nums[:k])
         max_num = sum_num
 
+        L = 0
         for i in range(k, len(nums)):
             sum_num += nums[i]
-            sum_num -= nums[i-k]
+            sum_num -= nums[L]
             max_num = max(max_num, sum_num)
+            L += 1
         
         return max_num / k
