fe34addc8ab65394854a78453127f342b8ecbb1a
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-08-03 16:54:15 -0700

    Time: 11 ms (82.53%), Space: 24 MB (9%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,12 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        
-        ans = defaultdict(list) #mapping character count to list of anagrams
-        
-        for s in strs:
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = collections.defaultdict(list)
+
+        for c in strs:
             count = [0] * 26
+            for i in c:
+                count[ord(i)-ord('a')] += 1
             
-            for c in s:
-                count[ord(c) - ord("a")] += 1
-                
-            ans[tuple(count)].append(s)
-        
-        return ans.values()
+            res[tuple(count)].append(c)
+
+        return list(res.values())
