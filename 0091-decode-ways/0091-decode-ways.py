8a16cb08ace6eb33ada9c786864b77c4e54a72fe
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-08-07 15:56:34 -0700

    Time: 0 ms (100%), Space: 19.6 MB (12.82%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,18 +1,18 @@
-class Solution(object):
-    def numDecodings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def numDecodings(self, s: str) -> int:
         dp = {len(s):1}
-        
-        for i in range(len(s)-1,-1,-1):
+
+        def dfs(i):
+            if i in dp:
+                return dp[i]
             if s[i] == "0":
-                dp[i] = 0
-            else:
-                dp[i] = dp[i+1]
+                return 0
             
-            if(1+i < len(s) and (s[i] == "1" or s[i] == "2" and s[1+i] in "0123456")):
-                dp[i] += dp[2+i]
+            res = dfs(i+1)
+            if (i + 1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i+1] in "0123456")):
+                res += dfs(i+2)
+            
+            dp[i] = res
+            return res
         
-        return dp[0]
+        return dfs(0)
