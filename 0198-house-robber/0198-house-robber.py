060305326861fc9887db84f8b03dfad95d1bca66
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-08-07 17:02:52 -0700

    Time: 0 ms (100%), Space: 19.3 MB (23.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,11 +3,11 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
 
-        dp = [0] * (len(nums)+1)
+        dp = [0] * (len(nums))
         dp[0] = nums[0]
         dp[1] = max(dp[0], nums[1])
 
         for i in range(2, len(nums)):
             dp[i] = max(dp[i-2] + nums[i], dp[i-1], nums[i])
         
-        return max(dp)
+        return dp[-1]
