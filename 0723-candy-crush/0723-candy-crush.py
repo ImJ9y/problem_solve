1a0c610a5a5e6fdb02d593f7e030d5a799b3a442
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-08-09 09:23:31 -0700

    Time: 51 ms (47.42%), Space: 19.3 MB (95.93%) - LeetHub

--- 0723-candy-crush/0723-candy-crush.py
+++ 0723-candy-crush/0723-candy-crush.py
@@ -4,28 +4,27 @@ class Solution:
         n = len(board[0])
         needCrush = False
 
-        #Vertically
         for i in range(m):
             for j in range(n-2):
                 if abs(board[i][j]) == abs(board[i][j+1]) == abs(board[i][j+2]) != 0:
                     board[i][j] = board[i][j+1] = board[i][j+2] = -abs(board[i][j])
                     needCrush = True
-        
-        #horizontally
+
         for i in range(m-2):
             for j in range(n):
                 if abs(board[i][j]) == abs(board[i+1][j]) == abs(board[i+2][j]) != 0:
                     board[i][j] = board[i+1][j] = board[i+2][j] = -abs(board[i][j])
                     needCrush = True
-        
+
+
         for j in range(n):
             anker_i = m - 1
 
-            for i in range(m-1, -1 ,-1):
+            for i in range(m-1,-1,-1):
                 if board[i][j] > 0:
                     board[anker_i][j] = board[i][j]
                     anker_i -= 1
-            
+        
             for i in range(anker_i + 1):
                 board[i][j] = 0
         
