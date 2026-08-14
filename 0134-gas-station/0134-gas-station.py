dbee12904325c12b8289efae50e357dab075494f
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-08-13 22:19:25 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,23 +1,13 @@
-class Solution(object):
-    def canCompleteCircuit(self, gas, cost):
-        """
-        :type gas: List[int]
-        :type cost: List[int]
-        :rtype: int
-        """
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         if sum(gas) < sum(cost):
             return -1
         
         start = 0
-        cur = 0
         for i in range(len(gas)):
-            cur += gas[i] - cost[i]
-            
-            if cur < 0:
-                cur = 0
-                start = i + 1
+            target = gas[i] - cost[i]
+
+            if target > start:
+                start = target + i
         
         return start
-            
-            
-                
