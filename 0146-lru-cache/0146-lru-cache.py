3bd8670342ac8b5ab4c57dcb86209c7af75032f9
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-08-13 22:25:19 -0700

    Time: 194 ms (7.27%), Space: 77.4 MB (31.75%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,18 +1,17 @@
-class Node:
+class Node():
     def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.next = self.prev = None
+        prev = next = None
 
 class LRUCache:
-
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {}
         self.left = self.right = Node(0,0)
         self.left.next = self.right
         self.right.prev = self.left
-
+        
     def insert(self, node):
         prev, next = self.right.prev, self.right
         prev.next, next.prev = node, node
@@ -27,14 +26,13 @@ class LRUCache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
-        
         return -1
         
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
-        
+
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
 
