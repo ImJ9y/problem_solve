73cf3ecf5754311400860a8f0dcaeeae93deba5e
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-08-15 14:02:54 -0700

    Time: 23 ms (67.23%), Space: 20 MB (44.88%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -2,22 +2,24 @@ class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         n = len(grid)
         minH = [[grid[0][0], 0, 0]] #time, r, c
-        coords = set()
+        directions = [(0,1),(1,0),(-1,0), (0,-1)]
         visit = set()
-        directions = [(-1,0),(1,0),(0,1),(0,-1)]
-
         visit.add((0,0))
+
         while minH:
             t, r, c = heapq.heappop(minH)
             visit.add((r,c))
+
             if r == n-1 and c == n-1:
                 return t
-            
-            for dr, dc in directions:
-                neiR, neiC = dr+r, dc+c
-                if neiR < 0 or neiC < 0 or neiR == n or neiC == n or (neiR, neiC) in visit:
-                    continue                
-                visit.add((neiR, neiC))
-                heapq.heappush(minH,[max(t,grid[neiR][neiC]), neiR, neiC])
 
+            for dr, dc in directions:
+                neiR = dr+r
+                neiC = dc+c
+
+                if neiR < 0 or neiC < 0 or neiR == n or neiC == n or (neiR, neiC) in visit:
+                    continue
+                
+                visit.add((neiR, neiC))
+                heapq.heappush(minH, [max(t, grid[neiR][neiC]), neiR, neiC])
                 
