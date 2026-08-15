2fa8c2b058341d456c5b352af8f81864671950bb
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-08-15 10:16:23 -0700

    Time: 7 ms (43.55%), Space: 21.4 MB (14.26%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -5,6 +5,7 @@ class Solution:
         for cur, preq in prerequisites:
             preq_map[cur].append(preq)
         
+
         visit = set()
         def dfs(cur):
             if cur in visit:
@@ -23,5 +24,5 @@ class Solution:
         for i in range(numCourses):
             if not dfs(i):
                 return False
-            
+        
         return True
