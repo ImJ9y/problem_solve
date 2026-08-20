5e13db4c1f52dab436c2931691bae9a9e76df7d3
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-08-19 19:30:53 -0700

    Time: 0 ms (100%), Space: 19.5 MB (39.41%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,23 +1,31 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        for i in range(len(matrix)):
-            L, R = 0, len(matrix[i])
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
 
-            while L < R:
-                M = (L+R)//2
+        L, R = 0, len(matrix)
+        col_L, col_R = 0, len(matrix[0])-1
+        while L <= R:
+            MID_ROW = (L + R) // 2
+            MID_COL = (col_L + col_R) // 2
 
-                if matrix[i][M] > target:
-                    R -= 1
-                elif matrix[i][M] < target:
-                    L += 1
-                else:
+            print(L, R)
+            print(col_L, col_R)
+
+            if MID_ROW < len(matrix) and MID_COL < len(matrix[0]) and matrix[MID_ROW][col_L] <= target and target <= matrix[MID_ROW][col_R]:
+                if matrix[MID_ROW][MID_COL] == target:
                     return True
+                elif matrix[MID_ROW][MID_COL] < target:
+                    col_L = MID_COL + 1
+                else:
+                    col_R = MID_COL - 1
+                print(matrix[MID_ROW][MID_COL])
+
+            elif MID_ROW < len(matrix) and MID_COL < len(matrix[0]) and matrix[MID_ROW][MID_COL] == target:
+                return True
+            elif MID_ROW < len(matrix) and MID_COL < len(matrix[0]) and matrix[MID_ROW][MID_COL] < target:
+                L = MID_ROW + 1
+                col_L, col_R = 0, len(matrix[0])-1
+            else:
+                R = MID_ROW - 1
+                col_L, col_R = 0, len(matrix[0])-1
         
         return False
-
-
