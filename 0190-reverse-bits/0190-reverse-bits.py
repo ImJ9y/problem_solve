0fa7f238ef79bec51b5e35ce2dcdce5d7bdee2e5
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-03 12:25:56 -0700

    Time: 51 ms (24.76%), Space: 19.1 MB (64.38%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,11 +1,18 @@
 class Solution:
-    # @param n, an integer
-    # @return an integer
-    def reverseBits(self, n):
+    def reverseBits(self, n: int) -> int:
+        bit = n
+        res = ""
+        while bit:
+            res += str(bit%2)
+            bit = bit//2
+
+        extra_zero = 32 - len(res)
+        res += '0' * extra_zero
+
         
         ans = 0
-        for i in range(32):
-            bit = (n >> i) & 1
-            ans = ans | (bit << (31-i))
-        
+        res_list = list(res)
+        for i in range(len(res_list)-1,-1,-1):
+            ans += int(res_list[i]) * (2 ** (31 - i))
+
         return ans
