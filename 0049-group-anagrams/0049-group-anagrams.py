2af8861c36831a3f35793d09c0835804c53e970b
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-07 04:34:07 -0700

    Time: 15 ms (41.24%), Space: 23.8 MB (26.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,7 +2,6 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         word_bank = defaultdict(list)
 
-
         for word in strs:
             count = [0] * 26
 
