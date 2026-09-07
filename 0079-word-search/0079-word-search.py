b3522f8e94f71ef4f268ce45f1c1da2b25ce889e
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-07 05:00:40 -0700

    Time: 4243 ms (34.75%), Space: 19.6 MB (8.66%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,6 +1,7 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLS = len(board), len(board[0])
+        
         path = set()
 
         def dfs(r, c, i):
@@ -11,14 +12,17 @@ class Solution:
                 return False
             
             path.add((r,c))
-            res = (dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c+1,i+1) or dfs(r,c-1,i+1))
+            res = (dfs(r+1,c,i+1) or
+            dfs(r-1,c,i+1) or
+            dfs(r,c+1,i+1) or
+            dfs(r,c-1,i+1))
             path.remove((r,c))
 
             return res
         
         for i in range(ROWS):
             for j in range(COLS):
-                if dfs(i, j, 0):
+                if dfs(i,j,0):
                     return True
         
         return False
