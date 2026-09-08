b7b0b77a54bccdcebd1052a62d4adeff72117ffd
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-08 14:56:14 -0700

    Time: 83 ms (60.04%), Space: 19.8 MB (16.02%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,9 +1,9 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
         max_freq = 0
         L = 0
         res = 0
+        count = {}
 
         for R in range(len(s)):
             count[s[R]] = 1 + count.get(s[R], 0)
@@ -12,7 +12,7 @@ class Solution:
             while R - L + 1 - max_freq > k:
                 count[s[L]] -= 1
                 L += 1
-            
+
             res = max(res, R - L + 1)
         
         return res
