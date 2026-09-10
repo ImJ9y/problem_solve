6c868104b20cee96973c0582ed30687d58f1f318
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-10 15:18:18 -0700

    Time: 3 ms (49.81%), Space: 27.8 MB (45.78%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -9,10 +9,11 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
         slow, fast = head, head.next
+
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-
+        
         second = slow.next
         prev = slow.next = None
 
@@ -30,3 +31,4 @@ class Solution:
             second.next = tmp1
 
             first, second = tmp1, tmp2
+        
