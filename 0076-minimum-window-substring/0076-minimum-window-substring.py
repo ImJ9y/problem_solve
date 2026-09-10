03762b6093b45a0083026b42377af286de2385fc
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-10 16:59:45 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,45 +1,33 @@
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
-        
-        if t == "": return ""
-
-        countT, window ={}, {}
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        count_T, window = {}, {}
 
         for c in t:
-            countT[c] = 1 + countT.get(c, 0)
+            count_T[c] = 1 + count_T.get(c, 0)
         
-        result, result_len = [-1, -1], float('inf')
-
-        have, need = 0, len(countT)
+        have, need = 0, len(count_T)
         L = 0
+        res, res_len = [-1,-1], float('inf')
 
         for R in range(len(s)):
             cur_c = s[R]
             window[cur_c] = 1 + window.get(cur_c, 0)
 
-            if cur_c in countT and window[cur_c] == countT[cur_c]:
+            if cur_c in count_T and window[cur_c] == count_T[cur_c]:
                 have += 1
-            
-            while need == have:
-                #result update
-                if R - L + 1 < result_len:
-                    result_len = R - L + 1
-                    result = [L, R]
 
-                #pop left
-                window[s[L]] -= 1
+                while have == need:
+                    if R - L + 1 < res_len:
+                        res = [L, R]
+                        res_len = R - L + 1
+                    
+                    window[s[L]] -= 1
+                    
+                    if s[L] in count_T and window[s[L]] < count_T[cur_c]:
+                        have -= 1
+                    
+                    L += 1
 
-                if s[L] in countT and window[s[L]] < countT[s[L]]:
-                    have -= 1
-            
-                L += 1
-        
-        L, R = result
+        L, R = res
 
-        return s[L:R+1] if result_len != float('inf') else ""
-            
+        return s[L:R+1] if res_len != float('inf') else ""
