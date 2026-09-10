0c3260f8358d45f405eee9fb533bf8090bbcdf3a
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-10 15:35:11 -0700

    Time: 52 ms (19.35%), Space: 19.1 MB (64.04%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,10 +1,11 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         reversed_bit = 0
+
         for _ in range(32):
-            reversed_bit = reversed_bit << 1
+            reversed_bit <<= 1
             bit = n & 1
             reversed_bit = reversed_bit | bit
-            n = n >> 1
+            n >>= 1
         
         return reversed_bit
