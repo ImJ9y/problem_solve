2b36b6bd806fc43423c12b9abae1fe3a9408e685
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-10 01:45:57 -0700

    Time: 11 ms (23.51%), Space: 20.9 MB (34.39%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,26 +4,27 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         ROWS, COLS = len(matrix), len(matrix[0])
-        rowZero = False
+        row_Zero = False
 
         for r in range(ROWS):
             for c in range(COLS):
                 if matrix[r][c] == 0:
                     matrix[0][c] = 0
+
                     if r > 0:
                         matrix[r][0] = 0
                     else:
-                        rowZero = True
-
+                        row_Zero = True
+        
         for r in range(1, ROWS):
             for c in range(1, COLS):
                 if matrix[r][0] == 0 or matrix[0][c] == 0:
                     matrix[r][c] = 0
-        
+            
         if matrix[0][0] == 0:
-            for R in range(ROWS):
-                matrix[R][0] = 0
+            for r in range(ROWS):
+                matrix[r][0] = 0
         
-        if rowZero:
+        if row_Zero:
             for c in range(COLS):
-                matrix[0][c] = 0 
+                matrix[0][c] = 0
