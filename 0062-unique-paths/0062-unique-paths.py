592deb08479a762577b175327ced6e810138263f
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-10 01:47:39 -0700

    Time: 0 ms (100%), Space: 19.3 MB (43.35%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -5,7 +5,7 @@ class Solution:
         for _ in range(m-1):
             new_rows = [1] * n
 
-            for i in range(n-2,-1,-1):
+            for i in range(n-2, -1, -1):
                 new_rows[i] = new_rows[i+1] + rows[i]
             
             rows = new_rows
