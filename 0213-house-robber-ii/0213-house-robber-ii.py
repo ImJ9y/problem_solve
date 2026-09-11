030e52b9520ac31ec6d89f028fd62c3555377138
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-10 20:06:49 -0700

    Time: 0 ms (100%), Space: 19.2 MB (68.95%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,19 +1,15 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         def helper(nums):
-            rob1, rob2 = 0,0
+            for i in range(1, len(nums)):
+                if i == 1:
+                    nums[i] = max(nums[i], nums[0])
+                else:
+                    nums[i] = max(nums[i] + nums[i-2], nums[i-1])
             
-            for num in nums:
-                temp = max(num+rob1, rob2)
-                rob1 = rob2
-                rob2 = temp
-            
-            return rob2
+            return nums[-1]
         
-        return max(nums[0], helper(nums[1:]), helper(nums[:-1]))
-        
+        if len(nums) == 1:
+            return nums[0]
+
+        return max(helper(nums[1:]), helper(nums[:-1]))
