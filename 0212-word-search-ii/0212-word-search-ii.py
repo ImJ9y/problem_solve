262c6e6643ff4e6ee578f816708f94f182560bab
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-10 20:41:30 -0700

    Time: 5769 ms (35.35%), Space: 21.7 MB (23.49%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -0,0 +1,45 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+    
+    def addWord(self, word):
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.isWord = True
+    
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+
+        for w in words:
+            root.addWord(w)
+
+        ROWS, COLS = len(board), len(board[0])
+        path = set()
+        res = set()
+
+        def dfs(r,c, node, word):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r,c) in path or board[r][c] not in node.children:
+                return False
+            
+            path.add((r,c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.isWord:
+                res.add(word)
+            
+            dfs(r+1,c,node,word)
+            dfs(r-1,c,node,word)
+            dfs(r,c+1,node,word)
+            dfs(r,c-1,node,word)
+            path.remove((r,c))
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r,c,root,"")
+        
+        return list(res)
