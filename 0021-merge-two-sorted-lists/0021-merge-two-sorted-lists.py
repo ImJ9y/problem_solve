4cd2caa9cd93ebc7057d9defdf45cbcdc24d1d89
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-11 15:58:38 -0700

    Time: 0 ms (100%), Space: 19.4 MB (7.28%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -9,22 +9,22 @@ class Solution:
         tail = dummy
 
         while list1 and list2:
-            if list1.val< list2.val:
-                tail.next = list1
+            if list1.val < list2.val:
+                tail.next= list1
                 list1 = list1.next
             else:
                 tail.next = list2
                 list2 = list2.next
             
-            tail =tail.next
+            tail = tail.next
         
         while list1:
-            tail.next = list1
+            tail.next =list1
             list1 = list1.next
             tail = tail.next
         
         while list2:
-            tail.next = list2
+            tail.next =list2
             list2 = list2.next
             tail = tail.next
         
