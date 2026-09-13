6da518174fe78181e98ebd817df3af5b4e89c170
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-12 19:42:53 -0700

    Time: 0 ms (100%), Space: 28.1 MB (6.8%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -30,6 +30,6 @@ class Solution:
             first.next = second
             second.next = tmp1
 
-            first, second = tmp1, tmp2
+            first, second = tmp1 , tmp2
         
         
