a832b2858c90961960383418ed6784fa9ae5bf96
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-12 23:23:52 -0700

    Time: 645 ms (49.22%), Space: 22.3 MB (32.39%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -12,6 +12,7 @@ class Solution:
 
             while L < R:
                 three_sum = nums[i] + nums[L] + nums[R]
+
                 if three_sum < 0:
                     L += 1
                 elif three_sum > 0:
@@ -21,7 +22,8 @@ class Solution:
 
                     while L < R and nums[L] == nums[L+1]:
                         L += 1
-                    while R > 0 and nums[R] == nums[R-1]:
+                    
+                    while R < len(nums)-1 and nums[R] == nums[R-1]:
                         R -= 1
                     
                     L += 1
