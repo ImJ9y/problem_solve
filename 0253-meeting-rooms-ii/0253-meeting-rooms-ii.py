5ecb98f80384a70b2bccdff4c67636a8202e76e9
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-16 14:36:39 -0700

    Time: 2 ms (88.82%), Space: 21.3 MB (79.78%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,14 +1,14 @@
 class Solution:
-    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+    def minMeetingRooms(self, intervals: list[list[int]]) -> int:
         start_time = sorted(start for start, end in intervals)
         end_time = sorted(end for start, end in intervals)
 
-        room = 0
+        res = 0
         idx = 0
         for start in start_time:
             if start < end_time[idx]:
-                room += 1
+                res += 1
             else:
                 idx += 1
-        
-        return room
+            
+        return res
