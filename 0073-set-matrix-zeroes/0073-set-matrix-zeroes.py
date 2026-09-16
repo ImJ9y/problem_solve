0cd847ccd136c45e09597911b9f55668e0a91320
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-16 14:29:14 -0700

    Time: 7 ms (56.83%), Space: 20.8 MB (38.16%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,5 +1,5 @@
 class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
+    def setZeroes(self, matrix: list[list[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
@@ -9,7 +9,6 @@ class Solution:
         for r in range(ROWS):
             for c in range(COLS):
                 if matrix[r][c] == 0:
-                    
                     matrix[0][c] = 0
 
                     if r > 0:
@@ -19,15 +18,13 @@ class Solution:
         
         for r in range(1, ROWS):
             for c in range(1, COLS):
-                if matrix[r][0] == 0 or matrix[0][c] == 0 :
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
                     matrix[r][c] = 0
         
-
         if matrix[0][0] == 0:
             for r in range(ROWS):
                 matrix[r][0] = 0
         
-
         if rowZero:
             for c in range(COLS):
                 matrix[0][c] = 0
