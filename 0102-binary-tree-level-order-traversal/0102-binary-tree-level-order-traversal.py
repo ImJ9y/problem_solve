583af6676974a3d667f44cc87ed28e05ccc25211
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-17 00:00:20 -0700

    Time: 2 ms (22.21%), Space: 20 MB (61.92%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,23 +6,44 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: TreeNode | None) -> list[list[int]]:
+        # if not root: return []
+
+        # stack = [(root, 0)]
+        # res = []
+
+        # while stack:
+        #     node, depth = stack.pop()
+
+        #     if len(res) == depth:
+        #         res.append([])
+            
+        #     res[depth].append(node.val)
+
+        #     if node.right:
+        #         stack.append((node.right, depth+1))
+            
+        #     if node.left:
+        #         stack.append((node.left, depth+1))
+        
+        # return res
+
         if not root: return []
 
-        stack = [(root, 0)]
+        queue = collections.deque([root])
+        depth = 0
         res = []
 
-        while stack:
-            node, depth = stack.pop()
-
-            if len(res) == depth:
-                res.append([])
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
             
-            res[depth].append(node.val)
-
-            if node.right:
-                stack.append((node.right, depth+1))
-            
-            if node.left:
-                stack.append((node.left, depth+1))
+            res.append(level)
         
         return res
