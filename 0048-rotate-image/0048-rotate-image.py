bfeb727711a8a7ffa2ca3c1449ecb5f89c72494e
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-19 22:51:10 -0700

    Time: 0 ms (100%), Space: 19.4 MB (6.36%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,5 +1,5 @@
 class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
+    def rotate(self, matrix: list[list[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
@@ -14,6 +14,6 @@ class Solution:
                 matrix[BOTTOM-i][LEFT] = matrix[BOTTOM][RIGHT-i]
                 matrix[BOTTOM][RIGHT-i] = matrix[TOP+i][RIGHT]
                 matrix[TOP+i][RIGHT] = TOPLEFT
-
+            
             LEFT += 1
             RIGHT -= 1
