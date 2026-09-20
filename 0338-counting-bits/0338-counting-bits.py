a0cd621a74e8c80c73b0f9bb6064b7f0e1a1d5cf
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-19 23:37:23 -0700

    Time: 3 ms (95.53%), Space: 20.1 MB (69.57%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,17 +1,24 @@
 class Solution:
     def countBits(self, n: int) -> list[int]:
-        def helper(n):
-            count = 0
-            while n:
-                if n & 1:
-                    count += 1
+        # def helper(n):
+        #     count = 0
+        #     while n:
+        #         if n & 1:
+        #             count += 1
                 
-                n >>= 1
+        #         n >>= 1
             
-            return count
+        #     return count
         
+        # res = [0]
+        # for i in range(1, n+1):
+        #     res.append(helper(i))
+        
+        # return res
+
         res = [0]
+
         for i in range(1, n+1):
-            res.append(helper(i))
+            res.append(res[i//2] + i%2)
         
         return res
