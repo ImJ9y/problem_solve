13dfad9d6cff918c2418013b0ecebe2651911579
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2026-09-22 10:48:39 -0700

    Time: 0 ms (100%), Space: 19.4 MB (17.9%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -1,28 +1,31 @@
 class Solution:
-    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+    def fullJustify(self, words: list[str], maxWidth: int) -> list[str]:
+        res = []
         line, length = [], 0
         i = 0
-        res = []
 
         while i < len(words):
-            if length + len(words[i]) + len(line) > maxWidth:
+
+            if length + len(line) + len(words[i]) > maxWidth:
+                
                 extra_spaces = maxWidth - length
                 spaces = extra_spaces // max(1, len(line)-1)
                 remainder = extra_spaces % max(1, len(line)-1)
 
                 for j in range(max(1, len(line)-1)):
                     line[j] += ' ' * spaces
+
                     if remainder:
                         line[j] += ' '
                         remainder -= 1
                 
                 res.append("".join(line))
                 line, length = [], 0
-            
+
             line.append(words[i])
             length += len(words[i])
             i += 1
-        
+
         last_word = " ".join(line)
         spaces = maxWidth - len(last_word)
         res.append(last_word + ' ' * spaces)
